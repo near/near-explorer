6788d2261587cb3659cce9382f486d9d32e79b8a
Robin Thomas <robinthomas2591@gmail.com>
2019-06-16 10:16:00 +0800

    create the search component

---
+++ components/utils/Search.js
@@ -0,0 +1,49 @@
+import { Row, Col } from "react-bootstrap";
+
+const Search = props => (
+  <div className="search">
+    <Row noGutters="true">
+      <Col md="1" className="align-self-center">
+        <img src="/static/images/icon-search.svg" className="search-icon" />
+      </Col>
+      <Col md="11" className="align-self-center">
+        <input
+          type="text"
+          className="search-text"
+          placeholder={props.text}
+          onChange={props.handler}
+        />
+      </Col>
+    </Row>
+    <style jsx global>{`
+      .search {
+        border-radius: 25px;
+        border: solid 2px #e6e6e6;
+        background-color: #f8f8f8;
+        padding: 4px 14px;
+      }
+
+      .search-icon {
+        width: 14px;
+      }
+
+      .search-text {
+        font-family: BentonSans;
+        font-size: 14px;
+        color: #999999;
+        outline: none;
+        border: 0;
+        background-color: #f8f8f8;
+        width: 100%;
+        font-weight: 100;
+      }
+
+      .search-text::placeholder {
+        color: #999999;
+        opacity: 1; /* Firefox */
+      }
+    `}</style>
+  </div>
+);
+
+export default Search;
