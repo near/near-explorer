2bf6cd5a5305cf1b47649a5641572c6c17f0aedb
Vlad Frolov <frolvlad@gmail.com>
2019-07-10 10:11:33 +0300

    Set up a runtime configuration on the Frontend subproject

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,19 @@
 // next.config.js
 const withCSS = require("@zeit/next-css");
 
+const defaultWampNearExplorerUrl = "ws://localhost:8080/ws";
+const defaultWampNearExplorerFrontendSecret = "front";
+
 module.exports = withCSS({
   webpack(config, options) {
     return config;
+  },
+  serverRuntimeConfig: {
+    wampNearExplorerUrl: process.env.WAMP_NEAR_EXPLORER_INTERNAL_URL || process.env.WAMP_NEAR_EXPLORER_URL || defaultWampNearExplorerUrl,
+    wampNearExplorerFrontendSecret: process.env.WAMP_NEAR_EXPLORER_INTERNAL_FRONTEND_SECRET || process.env.WAMP_NEAR_EXPLORER_FRONTEND_SECRET || defaultWampNearExplorerFrontendSecret
+  },
+  publicRuntimeConfig: {
+    wampNearExplorerUrl: process.env.WAMP_NEAR_EXPLORER_URL || defaultWampNearExplorerUrl,
+    wampNearExplorerFrontendSecret: process.env.WAMP_NEAR_EXPLORER_FRONTEND_SECRET || defaultWampNearExplorerFrontendSecret
   }
 });
