c0931385d670b92f394482b791089e72944c163d
Robin Thomas <robinthomas2591@gmail.com>
2019-07-15 21:34:15 +0800

    Use better page structure for blocks & transactions

--- frontend/pages/blocks/index.js
+++ frontend/pages/blocks/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 
-import Header from "../components/Header";
-import Transactions from "../components/Transactions";
-import Footer from "../components/Footer";
+import Header from "../../components/Header";
+import Blocks from "../../components/Blocks";
+import Footer from "../../components/Footer";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -10,10 +10,10 @@ export default () => (
   <div>
     <Head>
       <link rel="shortcut icon" type="image/png" href="/static/favicon.ico" />
-      <title>Near Explorer | Transactions</title>
+      <title>Near Explorer | Blocks</title>
     </Head>
     <Header />
-    <Transactions />
+    <Blocks />
     <Footer />
     <style jsx global>{`
       body {
