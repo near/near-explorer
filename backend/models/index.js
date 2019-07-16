6b58b9ef320f2c6ecffd57b4d51105e8cfe1e6e6
Vlad Frolov <frolvlad@gmail.com>
2019-07-16 19:01:21 +0300

    Refactored (simplified) read-only database setup for com.nearprotocol.explorer.select

--- backend/models/index.js
+++ backend/models/index.js
@@ -8,17 +8,29 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    ...config,
+    logging: false
+  }
+);
+
+const sequelizeReadOnly = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    ...config,
+    dialectOptions: {
+      ...config.dialectOptions,
+      mode: 1
+    },
+    logging: false
+  }
+);
 
 fs.readdirSync(__dirname)
   .filter(file => {
@@ -38,6 +50,7 @@ Object.keys(db).forEach(modelName => {
 });
 
 db.sequelize = sequelize;
+db.sequelizeReadOnly = sequelizeReadOnly;
 db.Sequelize = Sequelize;
 
 module.exports = db;
