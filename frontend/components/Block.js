33bdbc5eeae015531f4085cc9254cc474132a2af
Robin Thomas <robinthomas2591@gmail.com>
2019-07-21 10:46:27 +0800

    Make the block page

---
+++ frontend/components/Block.js
@@ -0,0 +1,181 @@
+import Link from "next/link";
+
+import moment from "moment";
+import { Row, Col, Card } from "react-bootstrap";
+
+import Content from "./Content";
+import EmptyRow from "./utils/EmptyRow";
+
+const BlockCard = props => (
+  <Card
+    className={props.cls}
+    style={{ border: "solid 4px #e6e6e6", borderRadius: "0" }}
+  >
+    <Card.Body>
+      <Row noGutters="true">
+        <Col xs="auto" md="12" className="block-card-title align-self-center">
+          {props.imgLink !== undefined ? (
+            <img src={props.imgLink} className="block-card-title-img" />
+          ) : null}
+          {props.title}
+        </Col>
+        <Col
+          xs="auto"
+          md="12"
+          className={`ml-auto block-card-text align-self-center ${
+            props.textCls !== undefined ? props.textCls : ""
+          }`}
+        >
+          {props.textLink === true ? (
+            <Link href={`/blocks/${props.text}`}>
+              <a className={props.textCls}>
+                {props.format !== undefined
+                  ? parseInt(props.text).toLocaleString()
+                  : props.text}
+              </a>
+            </Link>
+          ) : props.format !== undefined ? (
+            parseInt(props.text).toLocaleString()
+          ) : (
+            props.text
+          )}
+        </Col>
+      </Row>
+    </Card.Body>
+    <style jsx global>{`
+      .block-card-title {
+        text-transform: uppercase;
+        letter-spacing: 1.8px;
+        color: #999999;
+        font-family: BentonSans;
+        font-size: 14px;
+        font-weight: 500;
+      }
+
+      .block-card-title-img {
+        width: 12px !important;
+        margin-right: 8px;
+        margin-top: -3px;
+      }
+
+      .block-card-text {
+        font-family: BwSeidoRound;
+        font-size: 24px;
+        font-weight: 500;
+        color: #24272a;
+      }
+    `}</style>
+  </Card>
+);
+
+const Block = ({ block }) => {
+  return (
+    <Content title={`Block #${block.height}`} border={false}>
+      <Row noGutters="true">
+        <Col className="block-info-container">
+          <Row noGutters="true">
+            <Col md="3">
+              <BlockCard
+                title="Transactions"
+                imgLink="/static/images/icon-m-transaction.svg"
+                text={block.transactionsCount}
+                cls="block-card-no-side-border block-card-no-top-border"
+              />
+            </Col>
+            <Col md="2">
+              <BlockCard
+                title="Size"
+                imgLink="/static/images/icon-m-size.svg"
+                text="TODO"
+                cls="block-card-no-top-border"
+              />
+            </Col>
+            <Col md="7">
+              <BlockCard
+                title="Author"
+                imgLink="/static/images/icon-m-user.svg"
+                text={block.authorId}
+                cls="block-card-no-side-border block-card-no-top-border"
+              />
+            </Col>
+          </Row>
+          <Row noGutters="true">
+            <Col md="4">
+              <BlockCard
+                title="Created"
+                text={moment(parseInt(block.timestamp.substring(0, 13))).format(
+                  "MMMM DD, YYYY [at] h:mm:ssa"
+                )}
+                textCls="block-card-created-text"
+                cls="block-card-no-border"
+              />
+            </Col>
+            <Col md="8">
+              <BlockCard
+                title="Hash"
+                text={block.hash}
+                textCls="block-card-created-text"
+                cls="block-card-no-border"
+              />
+            </Col>
+          </Row>
+          <Row noGutters="true">
+            <Col md="12">
+              <BlockCard
+                title="Parent Hash"
+                text={block.hash}
+                textCls="block-card-parent-hash-text"
+                textLink={true}
+                cls="block-card-no-side-border block-card-no-bottom-border"
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <EmptyRow rows="5" />
+      <style jsx global>{`
+        .block-card-created-text {
+          font-size: 18px;
+          font-weight: 500;
+          color: #4a4f54;
+        }
+
+        .block-card-parent-hash-text {
+          font-size: 18px;
+          font-weight: 500;
+          color: #6ad1e3;
+          cursor: pointer;
+          text-decoration: none !important;
+        }
+
+        .block-card-parent-hash-text:hover {
+          color: #6ad1e3;
+        }
+
+        .block-card-no-border {
+          border: 0 !important;
+        }
+
+        .block-card-no-side-border {
+          border-left: 0 !important;
+          border-right: 0 !important;
+        }
+
+        .block-card-no-top-border {
+          border-top: 0 !important;
+        }
+
+        .block-card-no-bottom-border {
+          border-bottom: 0 !important;
+        }
+
+        .block-info-container {
+          border: solid 4px #e6e6e6;
+          border-radius: 4px;
+        }
+      `}</style>
+    </Content>
+  );
+};
+
+export default Block;
--- frontend/components/api/Blocks.js
+++ frontend/components/api/Blocks.js
@@ -17,7 +17,30 @@ const Blocks = {
         }
       ]);
     } catch (error) {
-      console.error("DataProvider.getBlocksInfo[] failed to fetch data due to:");
+      console.error(
+        "DataProvider.getBlocksInfo[] failed to fetch data due to:"
+      );
+      console.error(error);
+      throw error;
+    }
+  },
+
+  getBlockInfo: async hash => {
+    try {
+      const block = await call(".select", [
+        `SELECT blocks.hash, blocks.height, blocks.timestamp, blocks.author_id as authorId, COUNT(transactions.hash) as transactionsCount
+          FROM blocks
+          LEFT JOIN chunks ON chunks.block_hash = blocks.hash
+          LEFT JOIN transactions ON transactions.chunk_hash = chunks.hash
+          WHERE blocks.hash = :hash`,
+        {
+          hash: hash
+        }
+      ]);
+
+      return block[0];
+    } catch (error) {
+      console.error("DataProvider.getBlockInfo failed to fetch data due to:");
       console.error(error);
       throw error;
     }
