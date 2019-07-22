39fd1e7e2ae384cc1df35f7fbfa7add5699dce37
Vlad Frolov <frolvlad@gmail.com>
2019-07-22 20:25:36 +0200

    Implemented a re-fetch logic for the blocks that were missing due to various reasons

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,10 @@ const nearRpc = new nearlib.providers.JsonRpcProvider(nearRpcUrl);
 const syncFetchQueueSize = process.env.NEAR_SYNC_FETCH_QUEUE_SIZE || 1000;
 const syncSaveQueueSize = process.env.NEAR_SYNC_SAVE_QUEUE_SIZE || 10;
 const bulkDbUpdateSize = process.env.NEAR_SYNC_BULK_DB_UPDATE_SIZE || 10;
+const regularSyncNewNearcoreStateInterval =
+  process.env.NEAR_REGULAR_SYNC_NEW_NEARCORE_STATE_INTERVAL || 1000;
+const regularSyncMissingNearcoreStateInterval =
+  process.env.NEAR_REGULAR_SYNC_MISSING_NEARCORE_STATE_INTERVAL || 60000;
 
 const wamp = new autobahn.Connection({
   realm: "near-explorer",
@@ -66,55 +70,61 @@ function setupWamp() {
 
 async function saveBlocks(blocksInfo) {
   try {
-    await models.Block.bulkCreate(
-      blocksInfo.map(blockInfo => {
-        return {
-          hash: bs58.encode(Buffer.from(blockInfo.header.hash)),
-          height: blockInfo.header.height,
-          prevHash: bs58.encode(Buffer.from(blockInfo.header.prev_hash)),
-          timestamp: blockInfo.header.timestamp,
-          weight: blockInfo.header.total_weight.num,
-          authorId: "n/a", // TODO
-          listOfApprovals: "n/a" // TODO
-        };
-      })
-    );
+    await models.sequelize.transaction(async transaction => {
+      try {
+        await models.Block.bulkCreate(
+          blocksInfo.map(blockInfo => {
+            return {
+              hash: bs58.encode(Buffer.from(blockInfo.header.hash)),
+              height: blockInfo.header.height,
+              prevHash: bs58.encode(Buffer.from(blockInfo.header.prev_hash)),
+              timestamp: blockInfo.header.timestamp,
+              weight: blockInfo.header.total_weight.num,
+              authorId: "n/a", // TODO
+              listOfApprovals: "n/a" // TODO
+            };
+          })
+        );
 
-    // XXX: Chunks are not 1-to-1 matching with Blocks, but they are not ready in nearcore, yet.
-    await models.Chunk.bulkCreate(
-      blocksInfo.map(blockInfo => {
-        return {
-          hash: bs58.encode(Buffer.from(blockInfo.header.hash)),
-          blockHash: bs58.encode(Buffer.from(blockInfo.header.hash)),
-          shardId: "n/a",
-          authorId: "n/a"
-        };
-      })
-    );
+        // XXX: Chunks are not 1-to-1 matching with Blocks, but they are not ready in nearcore, yet.
+        await models.Chunk.bulkCreate(
+          blocksInfo.map(blockInfo => {
+            return {
+              hash: bs58.encode(Buffer.from(blockInfo.header.hash)),
+              blockHash: bs58.encode(Buffer.from(blockInfo.header.hash)),
+              shardId: "n/a",
+              authorId: "n/a"
+            };
+          })
+        );
 
-    await Promise.all(
-      blocksInfo
-        .filter(blockInfo => blockInfo.transactions.length > 0)
-        .map(blockInfo => {
-          models.Transaction.bulkCreate(
-            blockInfo.transactions.map(tx => {
-              const kind = Object.keys(tx.body)[0];
-              const args = tx.body[kind];
-              return {
-                hash: bs58.encode(Buffer.from(tx.hash)),
-                originator: args.originator,
-                destination: "n/a", // TODO
-                kind,
-                args,
-                parentHash: null, // TODO
-                chunkHash: blockInfo.hash, // TODO: use real chunk hash instead of block hash
-                status: "Completed", // TODO
-                logs: "" // TODO
-              };
+        await Promise.all(
+          blocksInfo
+            .filter(blockInfo => blockInfo.transactions.length > 0)
+            .map(blockInfo => {
+              models.Transaction.bulkCreate(
+                blockInfo.transactions.map(tx => {
+                  const kind = Object.keys(tx.body)[0];
+                  const args = tx.body[kind];
+                  return {
+                    hash: bs58.encode(Buffer.from(tx.hash)),
+                    originator: args.originator,
+                    destination: "n/a", // TODO
+                    kind,
+                    args,
+                    parentHash: null, // TODO
+                    chunkHash: blockInfo.hash, // TODO: use real chunk hash instead of block hash
+                    status: "Completed", // TODO
+                    logs: "" // TODO
+                  };
+                })
+              );
             })
-          );
-        })
-    );
+        );
+      } catch (error) {
+        console.warn("Failed to save a bulk of blocks due to ", error);
+      }
+    });
   } catch (error) {
     console.warn("Failed to save a bulk of blocks due to ", error);
   }
@@ -149,7 +159,9 @@ async function syncNearcoreBlocks(topBlockHeight, bottomBlockHeight) {
       await saves.shift();
     }
   }
-  saves.push(saveBlocks(await Promise.all(requests)));
+  let blocks = await Promise.all(requests);
+  blocks = blocks.filter(response => response !== null);
+  saves.push(saveBlocks(blocks));
   await Promise.all(saves);
 }
 
@@ -189,20 +201,84 @@ async function syncOldNearcoreState() {
   await syncNearcoreBlocks(oldestSyncedBlockHeight - 1, 1);
 }
 
+async function syncMissingNearcoreState() {
+  const latestSyncedBlock = await models.Block.findOne({
+    order: [["height", "DESC"]]
+  });
+  if (latestSyncedBlock === null) {
+    return;
+  }
+
+  const oldestSyncedBlock = await models.Block.findOne({ order: ["height"] });
+  if (oldestSyncedBlock === null) {
+    return;
+  }
+
+  const syncMissingNearcoreBlocks = async (lowHeight, highHeight) => {
+    if (lowHeight > highHeight) {
+      return;
+    }
+    const syncedBlocksCount = await models.Block.count({
+      where: {
+        height: {
+          [models.Sequelize.Op.between]: [lowHeight, highHeight]
+        }
+      }
+    });
+    if (highHeight - lowHeight + 1 === syncedBlocksCount) {
+      return;
+    }
+    if (
+      highHeight - lowHeight <= syncFetchQueueSize &&
+      syncedBlocksCount === 0
+    ) {
+      await syncNearcoreBlocks(highHeight, lowHeight);
+      return;
+    }
+    const midHeight = Math.floor((lowHeight + highHeight) / 2);
+    await syncMissingNearcoreBlocks(lowHeight, midHeight);
+    await syncMissingNearcoreBlocks(midHeight + 1, highHeight);
+  };
+
+  await syncMissingNearcoreBlocks(
+    oldestSyncedBlock.height + 1,
+    latestSyncedBlock.height - 1
+  );
+}
+
 async function syncFullNearcoreState() {
   await syncNewNearcoreState();
+  await syncMissingNearcoreState();
   await syncOldNearcoreState();
 }
 
 async function main() {
-  //syncFullNearcoreState();
+  syncFullNearcoreState();
 
   // TODO: we should publish (push) the information about the new blocks/transcations via WAMP.
   const regularSyncNewNearcoreState = async () => {
     await syncNewNearcoreState();
-    setTimeout(regularSyncNewNearcoreState, 1000);
+    setTimeout(
+      regularSyncNewNearcoreState,
+      regularSyncNewNearcoreStateInterval
+    );
   };
-  setTimeout(regularSyncNewNearcoreState, 10000);
+  setTimeout(
+    regularSyncNewNearcoreState,
+    regularSyncNewNearcoreStateInterval * 10
+  );
+
+  const regularSyncMissingNearcoreState = async () => {
+    await syncMissingNearcoreState();
+    setTimeout(
+      regularSyncMissingNearcoreState,
+      regularSyncMissingNearcoreStateInterval
+    );
+  };
+  setTimeout(
+    regularSyncMissingNearcoreState,
+    regularSyncMissingNearcoreStateInterval
+  );
 
   setupWamp();
 }
