7e3cafa6c0fd3da3780cb39a67346e5198e0d2f5
Robin Thomas <robinthomas2591@gmail.com>
2019-07-26 04:44:57 +0800

    Code fix

--- frontend/components/dashboard/DashboardTransactionsRow.js
+++ frontend/components/dashboard/DashboardTransactionsRow.js
@@ -3,13 +3,13 @@ import CreateAccount from "./transaction/CreateAccount";
 import FunctionCall from "./transaction/FunctionCall";
 import TransactionRow from "./transaction/transaction";
 
-const DashboardTransactionRow = props => {
-  const components = {
-    AddKey,
-    CreateAccount,
-    FunctionCall
-  };
+const components = {
+  AddKey,
+  CreateAccount,
+  FunctionCall
+};
 
+const DashboardTransactionRow = props => {
   const Component = components[props.txKind] || TransactionRow;
   return <Component {...props} />;
 };
