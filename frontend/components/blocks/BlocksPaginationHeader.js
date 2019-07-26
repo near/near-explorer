f6d4a7ef80a7e474e588bc533f74b358984c8a15
Robin Thomas <robinthomas2591@gmail.com>
2019-07-26 16:15:37 +0800

    Show total with pagination

--- frontend/components/blocks/BlocksPaginationHeader.js
+++ frontend/components/blocks/BlocksPaginationHeader.js
@@ -1,18 +1,31 @@
-import Link from "next/link";
-
 import { Row, Col } from "react-bootstrap";
 
 import { DataConsumer } from "../utils/DataProvider";
 import Search from "../utils/Search";
 
 const BlocksHeader = props => (
-  <div>
-    <Row>
-      <Col md="4" xs="6" className="ml-auto align-self-center">
-        <Search text="Search blocks..." />
-      </Col>
-    </Row>
-  </div>
+  <Row>
+    <Col md="auto" className="align-self-center pagination-total">
+      <DataConsumer>
+        {ctx =>
+          ctx.pagination.total ? ctx.pagination.total.toLocaleString() : 0
+        }
+      </DataConsumer>
+      &nbsp;Total
+    </Col>
+    <Col md="4" xs="6" className="ml-auto align-self-center">
+      <Search text="Search blocks..." />
+    </Col>
+    <style jsx global>{`
+      .pagination-total {
+        font-size: 12px;
+        font-weight: 500;
+        letter-spacing: 1.38px;
+        color: #999999;
+        text-transform: uppercase;
+      }
+    `}</style>
+  </Row>
 );
 
 export default BlocksHeader;
