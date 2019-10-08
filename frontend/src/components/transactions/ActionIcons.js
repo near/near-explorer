000309f3ce59ede0e41bff9319db3e5d0651d0a7
Vlad Frolov <frolvlad@gmail.com>
2019-10-08 09:29:21 +0300

    Migrated to Next.js 9.1.0 project structure with `src` and `public` folders

---
+++ frontend/src/components/transactions/ActionIcons.js
@@ -0,0 +1,19 @@
+import CreateAccount from "../../../public/static/images/icon-t-acct.svg";
+import DeleteAccount from "../../../public/static/images/icon-t-key-delete.svg";
+import DeployContract from "../../../public/static/images/icon-t-contract.svg";
+import FunctionCall from "../../../public/static/images/icon-t-call.svg";
+import Transfer from "../../../public/static/images/icon-t-transfer.svg";
+import Stake from "../../../public/static/images/icon-t-stake.svg";
+import AddKey from "../../../public/static/images/icon-t-key-new.svg";
+import DeleteKey from "../../../public/static/images/icon-t-key-delete.svg";
+
+export default {
+  CreateAccount,
+  DeleteAccount,
+  DeployContract,
+  FunctionCall,
+  Transfer,
+  Stake,
+  AddKey,
+  DeleteKey
+};
