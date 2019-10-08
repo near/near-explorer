56da4bb212138e9d8302184b88f7fea1cfd8ec10
Vlad Frolov <frolvlad@gmail.com>
2019-10-08 23:53:24 +0300

    Dropped hacky and useless EmptyRow

--- frontend/src/components/utils/PaginationSpinner.jsx
+++ frontend/src/components/utils/PaginationSpinner.jsx
@@ -1,10 +1,7 @@
 import { Row, Col, Spinner } from "react-bootstrap";
 
-import EmptyRow from "./EmptyRow";
-
 export default ({ hidden }) => (
   <div style={{ display: hidden ? "none" : "default" }}>
-    <EmptyRow />
     <Row>
       <Col md="auto" className="mx-auto">
         <Spinner animation="grow" />
