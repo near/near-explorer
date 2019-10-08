8f2b31ba44f5ea719fc48b3b04023ffd12282a8c
Kendall Cole <kcole16@users.noreply.github.com>
2019-10-08 11:22:22 -0700

    Update action message typos and formatting
    Small formatting changes and fixing "Transfered" typo

--- frontend/components/transactions/ActionMessage.js
+++ frontend/components/transactions/ActionMessage.js
@@ -2,29 +2,29 @@ import { formatNEAR } from "../utils/Balance";
 
 const transactionMessageRenderers = {
   CreateAccount: ({ transaction: { receiverId } }) =>
-    `New Account Created: @${receiverId}`,
+    `New account created: @${receiverId}`,
   DeleteAccount: ({ transaction: { receiverId } }) =>
-    `Account Deleted: @${receiverId}`,
+    `Account deleted: @${receiverId}`,
   DeployContract: ({ transaction: { receiverId } }) =>
-    `Contract Deployed: ${receiverId}`,
+    `Contract deployed: ${receiverId}`,
   FunctionCall: ({ transaction: { receiverId } }) =>
-    `Call: Called method in contract "${receiverId}"`,
+    `Called method in contract: "${receiverId}"`,
   Transfer: ({ transaction: { receiverId }, actionArgs: { deposit } }) =>
-    `Transfered ${formatNEAR(deposit)} Ⓝ to @${receiverId}`,
+    `Transferred ${formatNEAR(deposit)} Ⓝ to @${receiverId}`,
   Stake: ({ actionArgs: { stake, public_key } }) =>
     `Staked: ${formatNEAR(stake)} Ⓝ ${public_key.substring(0, 15)}...`,
   AddKey: ({ transaction: { receiverId }, actionArgs }) => {
     return typeof actionArgs.access_key.permission === "object"
-      ? `Access key for contract: "${
+      ? `Access key added for contract: "${
           actionArgs.access_key.permission.FunctionCall.receiver_id
         }"`
-      : `New Key Added for @${receiverId}: ${actionArgs.public_key.substring(
+      : `New key added for @${receiverId}: ${actionArgs.public_key.substring(
           0,
           15
         )}...`;
   },
   DeleteKey: ({ actionArgs: { public_key } }) =>
-    `Key Deleted: ${public_key.substring(0, 15)}...`
+    `Key deleted: ${public_key.substring(0, 15)}...`
 };
 
 export default props => {
