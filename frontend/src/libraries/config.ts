02e1c114b0e48a8c33c5625295d3fd3d8f56999b
Vlad Frolov <frolvlad@gmail.com>
2019-12-05 12:43:59 +0200

    Fixed PRs deployment on render.com (multi-network compatibility)

---
+++ frontend/src/libraries/config.ts
@@ -0,0 +1,19 @@
+import getConfig from "next/config";
+
+const {
+  publicRuntimeConfig: { nearNetworks, nearNetworkAliases }
+} = getConfig();
+
+export interface NearNetwork {
+  name: string;
+  explorerLink: string;
+  aliases: [string];
+}
+
+export function getNearNetwork(hostname: string): NearNetwork {
+  let nearNetwork = nearNetworkAliases[hostname];
+  if (nearNetwork === undefined) {
+    nearNetwork = nearNetworks[0];
+  }
+  return nearNetwork;
+}
