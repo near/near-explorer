3e04f59b4bff1273e8f9f4dee9ad445068bc0b98
Yifang Ma <yifang@nearprotocol.com>
2019-12-05 17:53:08 -0800

    change sequance

--- frontend/src/libraries/explorer-wamp/nodes.js
+++ frontend/src/libraries/explorer-wamp/nodes.js
@@ -19,6 +19,6 @@ export default class NodesApi extends ExplorerApi {
 //TODO: new query for telemetry data
 `SELECT agent_name as agentName, agent_version as agentVersion, agent_build as agentBuild, 
   ip_address as ipAddress, moniker, account_id as accountId, node_id as nodeId, 
-  peer_count as peerCount, is_validator as isValidator, last_seen as lastSeen, last_height as lastHeight
+  last_seen as lastSeen, last_height as lastHeight, peer_count as peerCount, is_validator as isValidator
     FROM nodes
     ORDER BY lastSeen DESC`;
