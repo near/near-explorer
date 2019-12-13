ffbc5d8fd1f50074b03204331d990199fbe84563
Vlad Frolov <frolvlad@gmail.com>
2019-12-13 15:30:14 +0200

    Fixed a failed TX showing as completed

---
+++ frontend/src/components/utils/ExecutionStatus.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { ExecutionStatus } from "../../libraries/explorer-wamp/transactions";
+
+const EXECUTION_STATUSES: Record<ExecutionStatus, string> = {
+  NotStarted: "Not started",
+  Started: "Started",
+  Failure: "Failed",
+  SuccessValue: "Succeeded"
+};
+
+export interface Props {
+  status: ExecutionStatus;
+}
+
+export default class extends React.Component<Props> {
+  render() {
+    return EXECUTION_STATUSES[this.props.status];
+  }
+}
