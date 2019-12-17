9b61f48824dab51c06e1b428e542cb103399300a
Yifang Ma <yifang@nearprotocol.com>
2019-12-17 12:03:02 -0800

    add component

--- frontend/src/components/accounts/Accounts.jsx
+++ frontend/src/components/accounts/Accounts.jsx
@@ -1,1 +1,57 @@
-export default () => <></>;
+import PaginationSpinner from "../utils/PaginationSpinner";
+
+import BlocksHeader from "./BlocksHeader";
+import BlocksList from "./BlocksList";
+
+export default class extends React.Component {
+  state = {
+    loading: false,
+    blocks: [],
+    pagination: {
+      start: null,
+      stop: null,
+      total: null,
+      newBlocks: 0,
+      count: 15,
+      search: ""
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <BlocksHeader
+          setLoading={this._setLoading}
+          setBlocks={this._setBlocks}
+          pagination={this.state.pagination}
+          setPagination={this._setPagination}
+        />
+        <BlocksList
+          blocks={this.state.blocks}
+          pagination={this.state.pagination}
+          loading={this.state.loading}
+          setLoading={this._setLoading}
+          setBlocks={this._setBlocks}
+          setPagination={this._setPagination}
+        />
+        <PaginationSpinner hidden={false} />
+      </>
+    );
+  }
+
+  _setLoading = loading => {
+    this.setState({ loading });
+  };
+
+  _setBlocks = callback => {
+    this.setState(state => {
+      return { ...state, blocks: callback(state.blocks) };
+    });
+  };
+
+  _setPagination = callback => {
+    this.setState(state => {
+      return { ...state, pagination: callback(state.pagination) };
+    });
+  };
+}
