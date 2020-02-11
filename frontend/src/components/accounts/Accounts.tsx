9a3047b90b8e47d6aaf4f849b84bc9db7dc93dd8
Yifang Ma <yifang@nearprotocol.com>
2020-02-10 17:27:28 -0800

    add pagination and list component

--- frontend/src/components/accounts/Accounts.tsx
+++ frontend/src/components/accounts/Accounts.tsx
@@ -1,117 +1,17 @@
 import React from "react";
 
-import AccountsApi, * as A from "../../libraries/explorer-wamp/accounts";
-
-import AccountsList from "./AccountsList";
-import PaginationSpinner from "../utils/PaginationSpinner";
-import FlipMove from "../utils/FlipMove";
+import Pagination from "../utils/Pagination";
 
 export interface Props {
   limit: number;
 }
 
-export interface State {
-  accounts: A.AccountBasicInfo[] | null;
-  stop: number;
-  loading: Boolean;
-}
-
-export default class extends React.Component<Props, State> {
+export default class extends React.Component<Props> {
   static defaultProps = {
     limit: 15
   };
 
-  state: State = {
-    accounts: null,
-    loading: false,
-    stop: this.props.limit
-  };
-
-  _accountApi: AccountsApi | null;
-  timer: ReturnType<typeof setTimeout> | null;
-  _accountLoader: Element | null;
-
-  constructor(props: Props) {
-    super(props);
-    this._accountApi = null;
-    this.timer = null;
-    this._accountLoader = null;
-  }
-
-  componentDidMount() {
-    this._accountApi = new AccountsApi();
-    document.addEventListener("scroll", this._onScroll);
-    this.timer = setTimeout(this.regularFetchInfo, 0);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("scroll", this._onScroll);
-    clearTimeout(this.timer!);
-    this.timer = null;
-  }
-
-  regularFetchInfo = async () => {
-    await this.fetchAccounts();
-    if (this.timer !== null) {
-      this.timer = setTimeout(this.regularFetchInfo, 10000);
-    }
-  };
-
-  fetchAccounts = async () => {
-    if (this._accountApi === null) {
-      this._accountApi = new AccountsApi();
-    }
-    const accounts = await this._accountApi.getAccounts(this.state.stop);
-    this.setState({ accounts });
-  };
-
-  _isAtBottom = () => {
-    return (
-      this._accountLoader &&
-      this._accountLoader.getBoundingClientRect().bottom <= window.innerHeight
-    );
-  };
-
-  _onScroll = async () => {
-    this._accountLoader = document.querySelector("#account");
-    const bottom = this._isAtBottom();
-    if (bottom) {
-      document.removeEventListener("scroll", this._onScroll);
-      await this._loadAccounts();
-      document.addEventListener("scroll", this._onScroll);
-    }
-  };
-
-  _loadAccounts = async () => {
-    this.setState({ stop: this.state.stop + this.props.limit, loading: true });
-    await this.fetchAccounts();
-    this.setState({ loading: false });
-  };
-
-  _getLength = async () => {
-    if (this._accountApi === null) {
-      this._accountApi = new AccountsApi();
-    }
-    const count = await this._accountApi.getAccountLength();
-    if (count <= this.state.stop) {
-      this.setState({ loading: false });
-    }
-  };
-
   render() {
-    const { accounts, loading } = this.state;
-    if (accounts === null) {
-      return <PaginationSpinner hidden={false} />;
-    }
-    return (
-      <>
-        <div id="account">
-          <FlipMove duration={1000} staggerDurationBy={0}>
-            <AccountsList accounts={accounts} />
-          </FlipMove>
-        </div>
-        {loading && <PaginationSpinner hidden={false} />}
-      </>
-    );
+    return <Pagination limit={this.props.limit} genre="Account" />;
   }
 }
--- frontend/src/components/transactions/Transactions.tsx
+++ frontend/src/components/transactions/Transactions.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import TransactionsApi, * as T from "../../libraries/explorer-wamp/transactions";
-import FlipMove from "../utils/FlipMove";
-import PaginationSpinner from "../utils/PaginationSpinner";
 
-import TransactionsList from "./TransactionsList";
+import Pagination from "../utils/Pagination";
 
 export interface Props {
   accountId?: string;
@@ -12,131 +9,20 @@ export interface Props {
   limit: number;
 }
 
-export interface State {
-  transactions: T.Transaction[] | null;
-  stop: number;
-  loading: Boolean;
-}
-
-export default class extends React.Component<Props, State> {
+export default class extends React.Component<Props> {
   static defaultProps = {
     reversed: false,
     limit: 15
   };
 
-  state: State = {
-    transactions: null,
-    stop: this.props.limit,
-    loading: false
-  };
-
-  _transactionsApi: TransactionsApi | null;
-  timer: ReturnType<typeof setTimeout> | null;
-  _transactionLoader: Element | null;
-
-  constructor(props: Props) {
-    super(props);
-
-    // TODO: Design ExplorerApi to handle server-side rendering gracefully.
-    this._transactionsApi = null;
-    this.timer = null;
-    this._transactionLoader = null;
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    if (this.props.accountId !== prevProps.accountId) {
-      this.setState({ transactions: null });
-    }
-  }
-
-  componentDidMount() {
-    this._transactionsApi = new TransactionsApi();
-    document.addEventListener("scroll", this._onScroll);
-    this.timer = setTimeout(this.regularFetchInfo, 0);
-    this._getLength();
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("scroll", this._onScroll);
-    clearTimeout(this.timer!);
-    this.timer = null;
-  }
-
-  regularFetchInfo = async () => {
-    await this.fetchTransactions();
-    if (this.timer !== null) {
-      this.timer = setTimeout(this.regularFetchInfo, 10000);
-    }
-  };
-
-  fetchTransactions = async () => {
-    if (this._transactionsApi === null) {
-      this._transactionsApi = new TransactionsApi();
-    }
-    const transactions = (await this._transactionsApi.getTransactions({
-      signerId: this.props.accountId,
-      receiverId: this.props.accountId,
-      blockHash: this.props.blockHash,
-      tail: this.props.reversed,
-      limit: this.state.stop
-    })) as T.Transaction[];
-    this.setState({ transactions });
-  };
-
-  _isAtBottom = () => {
-    return (
-      this._transactionLoader &&
-      this._transactionLoader.getBoundingClientRect().bottom <=
-        window.innerHeight
-    );
-  };
-
-  _onScroll = async () => {
-    this._transactionLoader = document.querySelector("#tx");
-    const bottom = this._isAtBottom();
-    if (bottom) {
-      document.removeEventListener("scroll", this._onScroll);
-      await this._loadTransactions();
-      document.addEventListener("scroll", this._onScroll);
-    }
-  };
-
-  _loadTransactions = async () => {
-    this.setState({ stop: this.state.stop + this.props.limit, loading: true });
-    await this.fetchTransactions();
-    this.setState({ loading: false });
-  };
-
-  _getLength = async () => {
-    let count;
-    if (this._transactionsApi === null) {
-      this._transactionsApi = new TransactionsApi();
-    }
-    if (this.props.accountId) {
-      count = await this._transactionsApi.getTXLength(this.props.accountId);
-      if (count <= this.state.stop) {
-        this.setState({ loading: false });
-      }
-    }
-  };
-
   render() {
-    const { transactions, loading } = this.state;
-    if (transactions === null) {
-      return <PaginationSpinner hidden={false} />;
-    }
     return (
-      <>
-        <div id="tx">
-          <FlipMove duration={1000} staggerDurationBy={0}>
-            <TransactionsList
-              transactions={transactions}
-              reversed={this.props.reversed}
-            />
-          </FlipMove>
-        </div>
-        {loading && <PaginationSpinner hidden={false} />}
-      </>
+      <Pagination
+        reversed={this.props.reversed}
+        limit={this.props.limit}
+        genre="Transaction"
+        accountId={this.props.accountId}
+      />
     );
   }
 }
