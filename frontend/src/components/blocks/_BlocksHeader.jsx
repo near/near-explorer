f5e9abc9857cdf7d9b1a88e59acb194ce8706cd7
Yifang Ma <yifang@nearprotocol.com>
2020-02-11 20:05:19 -0800

    unused name

---
+++ frontend/src/components/blocks/_BlocksHeader.jsx
@@ -0,0 +1,57 @@
+import { Row, Col } from "react-bootstrap";
+
+import BlocksApi from "../../libraries/explorer-wamp/blocks";
+
+import Search from "../utils/Search";
+
+export default ({ setLoading, setBlocks, pagination, setPagination }) => {
+  const search = async text => {
+    setLoading(true);
+
+    let blocks;
+    if (text === null || text === undefined || text.trim().length === 0) {
+      blocks = await new BlocksApi().getLatestBlocksInfo();
+    } else {
+      blocks = await new BlocksApi().searchBlocks(text);
+    }
+
+    setBlocks(() => blocks);
+
+    if (blocks.length > 0) {
+      setPagination(pagination => {
+        return {
+          ...pagination,
+          start: blocks[0].height,
+          stop: blocks[blocks.length - 1].height
+        };
+      });
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <Row>
+      <Col md="auto" className="align-self-center pagination-total">
+        {pagination.total && `${pagination.total.toLocaleString()} Total`}
+      </Col>
+      <Col md="5" xs="6" className="ml-auto align-self-center">
+        <Search
+          text="Search blocks..."
+          handler={search}
+          pagination={pagination}
+          setPagination={setPagination}
+        />
+      </Col>
+      <style jsx global>{`
+        .pagination-total {
+          font-size: 12px;
+          font-weight: 500;
+          letter-spacing: 1.38px;
+          color: #999999;
+          text-transform: uppercase;
+        }
+      `}</style>
+    </Row>
+  );
+};
---
+++ frontend/src/components/blocks/_BlocksList.jsx
@@ -0,0 +1,131 @@
+import LoadingOverlay from "react-loading-overlay";
+
+import BlocksApi from "../../libraries/explorer-wamp/blocks";
+
+import FlipMove from "../utils/FlipMove";
+import BlocksRow from "./BlocksRow";
+
+export default class extends React.Component {
+  componentDidMount() {
+    this._blocksApi = new BlocksApi();
+    this._blockLoader = document.getElementById("block-loader");
+    document.addEventListener("scroll", this._onScroll);
+
+    this._regularLoadBlocks();
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("scroll", this._onScroll);
+    clearTimeout(this.timer);
+    this.timer = null;
+  }
+
+  render() {
+    const { blocks } = this.props;
+    return (
+      <LoadingOverlay
+        active={this.props.loading}
+        spinner
+        text="Loading blocks..."
+      >
+        <div id="block-loader">
+          {blocks && (
+            <FlipMove
+              duration={1000}
+              staggerDurationBy={0}
+              style={{ minHeight: "300px" }}
+            >
+              {blocks.map(block => (
+                <BlocksRow key={block.hash + block.timestamp} block={block} />
+              ))}
+            </FlipMove>
+          )}
+        </div>
+      </LoadingOverlay>
+    );
+  }
+
+  _isAtBottom = () => {
+    return (
+      this._blockLoader.getBoundingClientRect().bottom <= window.innerHeight
+    );
+  };
+
+  _onScroll = async () => {
+    if (this._isAtBottom()) {
+      document.removeEventListener("scroll", this._onScroll);
+
+      await this._loadBlocks();
+
+      // Add the listener again.
+      document.addEventListener("scroll", this._onScroll);
+    }
+  };
+
+  _regularLoadBlocks = async () => {
+    await this._getTopBlocks();
+    if (this.timer !== null) {
+      this.timer = setTimeout(this._regularLoadBlocks, 10000);
+    }
+  };
+
+  _getTopBlocks = async () => {
+    const blocks = await this._blocksApi.getLatestBlocksInfo();
+    console.log(blocks[0]);
+    this.props.setBlocks(_blocks => {
+      _blocks = blocks;
+      return _blocks;
+    });
+    this.props.setPagination(pagination => {
+      return {
+        ...pagination,
+        stop: blocks[blocks.length - 1].height,
+        start: blocks[0].height
+      };
+    });
+  };
+
+  _loadBlocks = async () => {
+    let blocks;
+    if (this.props.pagination.stop === null) {
+      blocks = await this._blocksApi.getLatestBlocksInfo();
+    } else {
+      blocks = await this._getNextBatch(this.props.pagination);
+    }
+    if (blocks.length > 0) {
+      this.props.setBlocks(_blocks => {
+        _blocks.push(...blocks);
+        return _blocks;
+      });
+      this.props.setPagination(pagination => {
+        return { ...pagination, stop: blocks[blocks.length - 1].height };
+      });
+    }
+  };
+
+  _getNextBatch = async pagination => {
+    this.props.setLoading(true);
+
+    let blocks = [];
+    try {
+      if (pagination.search) {
+        blocks = await this._blocksApi.searchBlocks(
+          pagination.search,
+          pagination.stop
+        );
+      } else {
+        blocks = await this._blocksApi.getPreviousBlocks(
+          pagination.stop,
+          pagination.count
+        );
+      }
+    } catch (err) {
+      console.error("Blocks.getNextBatch failed to fetch data due to:");
+      console.error(err);
+    }
+
+    this.props.setLoading(false);
+
+    return blocks;
+  };
+}
