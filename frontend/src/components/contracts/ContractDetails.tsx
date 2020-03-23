76b46945e800cb3572516a0970277e0f264e4410
Yifang Ma <yifang@nearprotocol.com>
2020-03-23 10:13:40 -0700

    create contract detail component

--- frontend/src/components/contracts/ContractDetails.tsx
+++ frontend/src/components/contracts/ContractDetails.tsx
@@ -1,13 +1,13 @@
-// import moment from "moment";
+import moment from "moment";
 
-// import React from "react";
+import React from "react";
 
-// import { Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
-// import TransactionsApi, * as T from "../../libraries/explorer-wamp/transactions";
+import ContractsApi, * as C from "../../libraries/explorer-wamp/contracts";
 
-// import CardCell from "../utils/CardCell";
-// import TransactionLink from "../utils/TransactionLink";
+import CardCell from "../utils/CardCell";
+import TransactionLink from "../utils/TransactionLink";
 
 interface Props {
   accountId: string;
@@ -15,16 +15,69 @@ interface Props {
 
 interface State {
   display: boolean;
+  locked: boolean;
+  contractInfo: C.ContractInfo;
 }
 
 export default class extends React.Component<Props, State> {
   state: State = {
-    display: false
+    display: false,
+    locked: true,
+    contractInfo: {
+      id: "",
+      transactionHash: "",
+      timestamp: 0,
+      accessKeys: []
+    }
   };
 
-  checkTransactions = async () => {};
+  getContractInfo = async () => {
+    new ContractsApi()
+      .getContractInfo(this.props.accountId)
+      .then(contractInfo => {
+        console.log(contractInfo);
+      })
+      .catch(err => console.error(err));
+  };
+
+  componentDidMount() {
+    this.getContractInfo();
+  }
 
   render() {
-    return <></>;
+    return (
+      <>
+        <Row noGutters className="border-0">
+          <Col md="5">
+            <CardCell
+              title="Created"
+              text={
+                typeof account.timestamp === "number"
+                  ? moment(account.timestamp).format(
+                      "MMMM DD, YYYY [at] h:mm:ssa"
+                    )
+                  : account.timestamp
+              }
+              className="block-card-created account-card-back border-0"
+            />
+          </Col>
+          <Col md="7">
+            <CardCell
+              title="Creation Hash"
+              text={
+                account.address === "" ? (
+                  ""
+                ) : (
+                  <TransactionLink transactionHash={account.address}>
+                    {account.address}
+                  </TransactionLink>
+                )
+              }
+              className="block-card-created-text account-card-back border-0"
+            />
+          </Col>
+        </Row>
+      </>
+    );
   }
 }
