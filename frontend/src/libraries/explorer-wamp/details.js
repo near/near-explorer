ff976edb562d0eb6141ca12c19676af0836bb7a8
Vlad Frolov <frolvlad@gmail.com>
2020-03-27 21:45:13 -0300

    feature: Avoid using JOINs to get stats for the Dashboard (#234)
    ## Test Plan
    
    * Manually confirm the queries match the previous numbers

--- frontend/src/libraries/explorer-wamp/details.js
+++ frontend/src/libraries/explorer-wamp/details.js
@@ -10,18 +10,15 @@ export default class DetailsApi extends ExplorerApi {
           online_nodes.onlineNodesCount,
           transactions.lastDayTxCount,
           last_block.lastBlockHeight,
-          ((transactions_per_second.tx_last_10_seconds + 9) / 10) as transactionsPerSecond
+          ((transactions_per_second.transactionsPerLast10Seconds + 9) / 10) as transactionsPerSecond
         FROM
           (SELECT COUNT(*) as totalNodesCount FROM nodes) as nodes,
           (SELECT COUNT(*) as onlineNodesCount FROM nodes WHERE last_seen > (strftime('%s','now') - 60) * 1000) as online_nodes,
-          (
-            SELECT COUNT(*) as lastDayTxCount FROM transactions
-              LEFT JOIN blocks ON blocks.hash = transactions.block_hash
-              WHERE blocks.timestamp > (strftime('%s','now') - 60 * 60 * 24) * 1000
+          (SELECT COUNT(*) as lastDayTxCount FROM transactions
+              WHERE block_timestamp > (strftime('%s','now') - 60 * 60 * 24) * 1000
           ) as transactions,
-          (SELECT COUNT(transactions.hash) as tx_last_10_seconds FROM transactions
-              LEFT JOIN blocks ON blocks.hash = transactions.block_hash
-              WHERE blocks.timestamp > (strftime('%s','now') - 10) * 1000
+          (SELECT COUNT(*) as transactionsPerLast10Seconds FROM transactions
+              WHERE block_timestamp > (strftime('%s','now') - 10) * 1000
           ) as transactions_per_second,
           (SELECT height as lastBlockHeight FROM blocks ORDER BY height DESC LIMIT 1) as last_block,
           (SELECT COUNT(*) as accountsCount FROM accounts) as total_accounts`
