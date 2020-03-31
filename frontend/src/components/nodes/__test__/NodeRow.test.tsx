b4ad253ce754ab1859d7d3583fa07edac57a9183
Yifang Ma <33007476+icerove@users.noreply.github.com>
2020-03-31 15:23:26 -0700

    refactor noderow (#221)
    make noderow file to the tsx
    
    ## Test plan
    add noderow to have snapshot test

---
+++ frontend/src/components/nodes/__test__/NodeRow.test.tsx
@@ -0,0 +1,29 @@
+import renderer from "react-test-renderer";
+
+import NodeRow from "../NodeRow";
+
+describe("<NodeRow />", () => {
+  it("renders", () => {
+    expect(
+      renderer.create(
+        <NodeRow
+          node={{
+            ipAddress: "1.1.1.1",
+            moniker: "",
+            accountId: "",
+            nodeId: "",
+            lastSeen: 12345,
+            lastHeight: 12345,
+            lastHash: null,
+            signature: null,
+            agentName: null,
+            agentVersion: null,
+            agentBuild: null,
+            peerCount: null,
+            isValidator: null
+          }}
+        />
+      )
+    ).toMatchSnapshot();
+  });
+});
