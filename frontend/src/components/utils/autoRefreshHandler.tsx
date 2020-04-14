40e0546aa9bae61a67a108179ccc617ef567d743
Yifang Ma <33007476+icerove@users.noreply.github.com>
2020-04-13 18:10:23 -0700

    add button to load more (#248)
    add button load more to fetch more data when browser page is fit on the whole screen

--- frontend/src/components/utils/autoRefreshHandler.tsx
+++ frontend/src/components/utils/autoRefreshHandler.tsx
@@ -7,7 +7,7 @@ import PaginationSpinner from "./PaginationSpinner";
 interface Config {
   fetchDataFn: Function;
   count: number;
-  categary: string;
+  category: string;
   dashboard?: boolean;
 }
 
@@ -28,7 +28,8 @@ export default (
       items: Array<any>(),
       itemsLength: config.count,
       display: false,
-      hasMore: true
+      hasMore: true,
+      loading: false
     };
 
     componentDidMount() {
@@ -49,54 +50,80 @@ export default (
           items: Array<any>(),
           itemsLength: config.count,
           display: false,
-          hasMore: true
+          hasMore: true,
+          loading: false
         });
       }
     }
+
+    regularFetchInfo = () => {
+      if (config.dashboard) {
+        this.setState({ hasMore: false });
+      }
+      if (this.state.itemsLength > 0) {
+        this.fetchInfo(this.state.itemsLength);
+        if (this.timer !== null) {
+          this.timer = setTimeout(this.regularFetchInfo, 10000);
+        }
+      }
+    };
 
     fetchInfo = (count: number) => {
       config
         .fetchDataFn(count)
         .then((items: any) => {
+          let newState: any;
           if (items.length > 0) {
-            this.setState({ items, display: true });
+            newState = {
+              items,
+              itemsLength: items.length,
+              display: true
+            };
+            if (items.length < config.count) {
+              newState.hasMore = false;
+            }
           } else {
-            this.setState({ hasMore: false, display: true });
+            newState = {
+              hasMore: false,
+              display: true,
+              itemsLength: 0
+            };
           }
+          this.setState(newState);
         })
-        .catch((err: any) => {
+        .catch((err: Error) => {
           console.error(err);
         });
     };
 
-    regularFetchInfo = () => {
-      this.fetchInfo(this.state.itemsLength);
-      if (this.timer !== null) {
-        this.timer = setTimeout(this.regularFetchInfo, 10000);
-      }
-    };
-
     fetchMoreData = async () => {
-      if (config.dashboard) {
-        this.setState({ hasMore: false });
-        return;
-      }
-      if (this.state.items.length > 0) {
-        const endTimestamp = this.getEndTimestamp(config.categary);
-        const newData = await config.fetchDataFn(config.count, endTimestamp);
-        if (newData.length > 0) {
-          const items = this.state.items.concat(newData);
-          this.setState({ items, itemsLength: items.length });
-        } else {
-          this.setState({ hasMore: false });
-        }
-        return;
-      }
+      this.setState({ loading: true });
+      const endTimestamp = this.getEndTimestamp(config.category);
+      config
+        .fetchDataFn(config.count, endTimestamp)
+        .then((newData: any) => {
+          let newState: any;
+          if (newData.length > 0) {
+            const items = this.state.items.concat(newData);
+            newState = {
+              items,
+              itemsLength: items.length,
+              loading: false
+            };
+            if (newData.length < config.count) {
+              newState.hasMore = false;
+            }
+          } else {
+            newState = { hasMore: false, loading: false };
+          }
+          this.setState(newState);
+        })
+        .catch((err: Error) => console.error(err));
     };
 
-    getEndTimestamp = (categary: string) => {
+    getEndTimestamp = (category: string) => {
       let endTimestamp;
-      switch (categary) {
+      switch (category) {
         case "Account":
           endTimestamp = this.state.items[this.state.items.length - 1]
             .createdAtBlockTimestamp;
@@ -119,18 +146,54 @@ export default (
     };
 
     render() {
-      return this.state.display ? (
-        <InfiniteScroll
-          dataLength={this.state.items.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.hasMore}
-          loader={<p>Loading</p>}
-          style={{ overflowX: "hidden" }}
-        >
-          <WrappedComponent items={this.state.items} {...props} />
-        </InfiniteScroll>
-      ) : (
-        <PaginationSpinner hidden={false} />
+      if (!this.state.display) {
+        return <PaginationSpinner hidden={false} />;
+      }
+      return (
+        <>
+          <InfiniteScroll
+            dataLength={this.state.items.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.hasMore}
+            loader={
+              this.state.loading ? (
+                <PaginationSpinner hidden={false} />
+              ) : (
+                <>
+                  <button
+                    onClick={this.fetchMoreData}
+                    className="load-button"
+                    style={{ display: this.state.hasMore ? "block" : "none" }}
+                  >
+                    Load More
+                  </button>
+                </>
+              )
+            }
+            style={{ overflowX: "hidden" }}
+          >
+            <WrappedComponent items={this.state.items} {...props} />
+          </InfiniteScroll>
+          <style jsx global>{`
+            .load-button {
+              width: 100px;
+              background-color: #f8f8f8;
+              display: block;
+              text-align: center;
+              text-decoration: none;
+              font-family: BentonSans;
+              font-size: 14px;
+              color: #0072ce;
+              font-weight: bold;
+              text-transform: uppercase;
+              margin: 20px auto;
+              border-radius: 30px;
+              padding: 8px 0;
+              cursor: pointer;
+              border: none;
+            }
+          `}</style>
+        </>
       );
     }
   };
