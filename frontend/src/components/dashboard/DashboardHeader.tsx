771be5f7ab4f65178cf81b20efbc3895e9d7bcb7
Yifang Ma <yifang@nearprotocol.com>
2020-04-23 15:24:27 -0700

    dashboardHeaders finish

--- frontend/src/components/dashboard/DashboardHeader.tsx
+++ frontend/src/components/dashboard/DashboardHeader.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import Router from "next/router";
 
 import React from "react";
@@ -10,11 +9,13 @@ import TransactionsApi from "../../libraries/explorer-wamp/transactions";
 import DetailsApi, * as D from "../../libraries/explorer-wamp/details";
 
 import CardCell from "../utils/CardCell";
+import Modal from "../utils/Modal";
 
 interface State {
   details?: D.Details;
   searchValue: string;
   loading: boolean;
+  modalInfo?: string;
 }
 
 export default class DashboardHeader extends React.Component<State> {
@@ -27,7 +28,7 @@ export default class DashboardHeader extends React.Component<State> {
 
   state: State = {
     searchValue: "",
-    loading: true
+    loading: true,
   };
 
   handleSearch = async (event: any) => {
@@ -83,12 +84,12 @@ export default class DashboardHeader extends React.Component<State> {
   fetchInfo = async () => {
     new DetailsApi()
       .getDetails()
-      .then(details => {
+      .then((details) => {
         if (details !== undefined) {
           this.setState({ details, loading: false });
         }
       })
-      .catch(err => console.error(err));
+      .catch((err) => console.error(err));
   };
 
   regularFetchInfo = async () => {
@@ -98,31 +99,53 @@ export default class DashboardHeader extends React.Component<State> {
     }
   };
 
+  changeModalHandler = (name: string) => {
+    this.setState({ modalInfo: name });
+  };
+
+  ModalCloseHander = () => {
+    this.setState({ modalInfo: undefined });
+  };
+
   render() {
-    const { details, loading } = this.state;
+    const { details, loading, modalInfo } = this.state;
     return (
       <div className="dashboard-info-container">
         <Row noGutters>
           <Col xs="12" md="3">
-            <Link href="/nodes">
-              <a>
-                <CardCell
-                  title="Nodes Online"
-                  imgLink="/static/images/icon-m-node-online.svg"
-                  text={
-                    details
-                      ? `${details.onlineNodesCount.toLocaleString()}/${details.totalNodesCount.toLocaleString()}`
-                      : ""
-                  }
-                  className="border-0"
-                  loading={loading}
-                />
-              </a>
-            </Link>
+            <CardCell
+              title={
+                <>
+                  Nodes Online
+                  <img
+                    src="/static/images/icon-info.svg"
+                    className="info"
+                    onClick={() => this.changeModalHandler("Nodes")}
+                  />
+                </>
+              }
+              imgLink="/static/images/icon-m-node-online.svg"
+              text={
+                details
+                  ? `${details.onlineNodesCount.toLocaleString()}/${details.totalNodesCount.toLocaleString()}`
+                  : ""
+              }
+              className="border-0"
+              loading={loading}
+            />
           </Col>
           <Col xs="12" md="3">
             <CardCell
-              title="Block Height"
+              title={
+                <>
+                  Block Height
+                  <img
+                    src="/static/images/icon-info.svg"
+                    className="info"
+                    onClick={() => this.changeModalHandler("Block Height")}
+                  />
+                </>
+              }
               imgLink="/static/images/icon-m-height.svg"
               text={details ? details.lastBlockHeight.toLocaleString() : ""}
               loading={loading}
@@ -130,7 +153,16 @@ export default class DashboardHeader extends React.Component<State> {
           </Col>
           <Col xs="12" md="2">
             <CardCell
-              title="Tps"
+              title={
+                <>
+                  Tps
+                  <img
+                    src="/static/images/icon-info.svg"
+                    className="info"
+                    onClick={() => this.changeModalHandler("Tps")}
+                  />
+                </>
+              }
               imgLink="/static/images/icon-m-tps.svg"
               text={
                 details
@@ -144,23 +176,37 @@ export default class DashboardHeader extends React.Component<State> {
           </Col>
           <Col xs="12" md="2">
             <CardCell
-              title="Last Day Tx"
+              title={
+                <>
+                  Last Day Tx
+                  <img
+                    src="/static/images/icon-info.svg"
+                    className="info"
+                    onClick={() => this.changeModalHandler("Last Day Tx")}
+                  />
+                </>
+              }
               imgLink="/static/images/icon-m-transaction.svg"
               text={details ? details.lastDayTxCount.toLocaleString() : ""}
               loading={loading}
             />
           </Col>
           <Col xs="12" md="2">
-            <Link href="accounts">
-              <a>
-                <CardCell
-                  title="Accounts"
-                  imgLink="/static/images/icon-m-user.svg"
-                  text={details ? details.accountsCount.toLocaleString() : ""}
-                  loading={loading}
-                />
-              </a>
-            </Link>
+            <CardCell
+              title={
+                <>
+                  Accounts
+                  <img
+                    src="/static/images/icon-info.svg"
+                    className="info"
+                    onClick={() => this.changeModalHandler("Accounts")}
+                  />
+                </>
+              }
+              imgLink="/static/images/icon-m-user.svg"
+              text={details ? details.accountsCount.toLocaleString() : ""}
+              loading={loading}
+            />
           </Col>
         </Row>
         <form onSubmit={this.handleSearch}>
@@ -191,6 +237,62 @@ export default class DashboardHeader extends React.Component<State> {
             </Col>
           </Row>
         </form>
+        {modalInfo === "Nodes" && (
+          <Modal modalClosed={this.ModalCloseHander}>
+            <h4>Nodes Online</h4>
+            The number of validating nodes / the total number of online nodes.
+            See more details on{" "}
+            <a
+              href="https://docs.nearprotocol.com/docs/roles/integrator/faq#validators"
+              target="_blank"
+            >
+              docs
+            </a>
+          </Modal>
+        )}
+        {modalInfo === "Block Height" && (
+          <Modal modalClosed={this.ModalCloseHander}>
+            <h4>Block Height</h4>
+            Most recent block recorded to the blockchain.
+          </Modal>
+        )}
+        {modalInfo === "Tps" && (
+          <Modal modalClosed={this.ModalCloseHander}>
+            <h4>Tps</h4>
+            Average transactions per second (TPS) in the 10 most recent blocks.
+            See more details on{" "}
+            <a
+              href="https://docs.nearprotocol.com/docs/concepts/transaction"
+              target="_blank"
+            >
+              docs
+            </a>
+          </Modal>
+        )}
+        {modalInfo === "Last Day Tx" && (
+          <Modal modalClosed={this.ModalCloseHander}>
+            <h4>Last Day Tx</h4>
+            Total transactions in the last 24 hours. See more details on{" "}
+            <a
+              href="https://docs.nearprotocol.com/docs/concepts/transaction"
+              target="_blank"
+            >
+              docs
+            </a>
+          </Modal>
+        )}
+        {modalInfo === "Accounts" && (
+          <Modal modalClosed={this.ModalCloseHander}>
+            <h4>Accounts</h4>
+            Total number of accounts created. See more details on{" "}
+            <a
+              href="https://docs.nearprotocol.com/docs/concepts/account"
+              target="_blank"
+            >
+              docs
+            </a>
+          </Modal>
+        )}
         <style jsx global>{`
           .dashboard-info-container {
             border: solid 4px #e6e6e6;
@@ -223,6 +325,12 @@ export default class DashboardHeader extends React.Component<State> {
           }
           .button-search {
             border-radius: 25px;
+          }
+
+          .info {
+            margin-left: 5px;
+            width: 18px;
+            cursor: pointer;
           }
         `}</style>
       </div>
---
+++ frontend/src/components/utils/Modal.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default class extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="backdrop" onClick={this.props.modalClosed}></div>
+        <div className="Modal">
+          <img
+            src="/static/images/icon-close.svg"
+            className="close"
+            onClick={this.props.modalClosed}
+          />
+          {this.props.children}
+        </div>
+        <style jsx global>{`
+          .backdrop {
+            width: 100%;
+            height: 100%;
+            position: fixed;
+            z-index: 10;
+            left: 0;
+            top: 0;
+            background-color: rgba(0, 0, 0, 0.5);
+          }
+
+          .Modal {
+            position: fixed;
+            left: 35%;
+            z-index: 50;
+            text-align: left;
+            background-color: #fff;
+            width: 340px;
+            margin: 0;
+            border-radius: 5px;
+            box-shadow: 1px 3px 3px 0 rgba(0, 0, 0, 0.2),
+              1px 3px 15px 2px rgba(0, 0, 0, 0.2);
+            padding: 25px;
+            box-sizing: border-box;
+          }
+
+          .close {
+            width: 20px;
+            margin-left: 20px;
+            cursor: pointer;
+          }
+        `}</style>
+      </>
+    );
+  }
+}
