104e629a6524180329564a8ae9b5aefb9efc3ae5
Yifang Ma <yifang@nearprotocol.com>
2020-05-06 15:54:05 -0700

    change function name

--- frontend/src/components/utils/autoRefreshHandler.tsx
+++ frontend/src/components/utils/autoRefreshHandler.tsx
@@ -98,7 +98,7 @@ export default (
 
     fetchMoreData = async () => {
       this.setState({ loading: true });
-      const paginationIndexer = this.getEndTimestamp(config.category);
+      const paginationIndexer = this.getPaginationIndexer(config.category);
       config
         .fetchDataFn(config.count, paginationIndexer)
         .then((newData: any) => {
@@ -121,7 +121,7 @@ export default (
         .catch((err: Error) => console.error(err));
     };
 
-    getEndTimestamp = (category: string) => {
+    getPaginationIndexer = (category: string) => {
       let paginationIndexer;
       switch (category) {
         case "Account":
