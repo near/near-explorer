dbf144ae2cf74a8195fa69775191f16acf3ce138
Yifang Ma <yifang@nearprotocol.com>
2020-07-20 17:41:04 -0700

    add new tests

--- frontend/src/components/nodes/__test__/ProposalRow.test.tsx
+++ frontend/src/components/nodes/__test__/ProposalRow.test.tsx
@@ -1,19 +1,17 @@
 import renderer from "react-test-renderer";
 
-import ValidatorRow from "../ValidatorRow";
+import ProposalRow from "../ProposalRow";
 
-describe("<ValidatorRow />", () => {
+describe("<ProposalRow />", () => {
   it("renders", () => {
     expect(
       renderer.create(
-        <ValidatorRow
+        <ProposalRow
           node={{
             account_id: "dokiacapital.pool.6fb1358",
-            is_slashed: false,
-            num_expected_blocks: 1771,
-            num_produced_blocks: 1768,
+
             public_key: "ed25519:935JMz1vLcJxFApG3TY4MA4RHhvResvoGwCrQoJxHPn9",
-            shards: [0],
+
             stake: "91037770393145811562101780866",
           }}
         />
