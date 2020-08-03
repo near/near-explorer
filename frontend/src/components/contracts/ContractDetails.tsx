0bf178e11c00d5cf4360bcdfd708d611d8518682
Vlad Frolov <frolvlad@gmail.com>
2020-08-03 14:21:07 -0300

    fix(frontend): Clarified the documentation message for the locked state of a contract

--- frontend/src/components/contracts/ContractDetails.tsx
+++ frontend/src/components/contracts/ContractDetails.tsx
@@ -124,7 +124,8 @@ export default class extends React.Component<Props, State> {
                 title={
                   <Term title={"Locked?"}>
                     {`Locked contract means that there are no access keys allowing the contract code to be re-deployed 
-                (e.g. even a single FullAccess permission access key casts the locked status to "No"). `}
+                (e.g. even a single FullAccess permission access key casts the locked status to "No"). However, the
+                contract itself may still be implemented the way that it re-deploys itself or re-add FullAccess keys.`}
                   </Term>
                 }
                 text={lockedShow ? lockedShow : ""}
