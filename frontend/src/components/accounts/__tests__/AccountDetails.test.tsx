a923a621210e44e1a5fcdda3ddd82fef0353e378
Yifang Ma <yifang@nearprotocol.com>
2020-10-13 12:54:50 -0700

    update test

--- frontend/src/components/accounts/__tests__/AccountDetails.test.tsx
+++ frontend/src/components/accounts/__tests__/AccountDetails.test.tsx
@@ -9,16 +9,17 @@ describe("<AccountDetails />", () => {
         <AccountDetails
           account={{
             id: "account",
-            amount: "1",
-            locked: "1",
-            storageUsage: 0,
+            amount: "14567898765434567876543",
+            locked: "0",
+            storageUsage: 876,
             storagePaidAt: 1,
-            inTransactionsCount: 0,
-            outTransactionsCount: 0,
+            inTransactionsCount: 10,
+            outTransactionsCount: 7,
             createdAtBlockTimestamp: Number(new Date(2019, 1, 1)),
             createdByTransactionHash:
               "EVvWW1S9BFaEjY1JBNSdstb7ZTtTFjQ6cygkbw1KY4tL",
             accountIndex: 1234567890,
+            storageAmountPerByte: "1000000000000000000000",
           }}
         />
       )
