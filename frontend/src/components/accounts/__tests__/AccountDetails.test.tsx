11dc73d7c982f0ab7231a9580dd2c26468b88090
Yifang Ma <yifang@nearprotocol.com>
2020-10-16 14:35:12 -0700

    update test

--- frontend/src/components/accounts/__tests__/AccountDetails.test.tsx
+++ frontend/src/components/accounts/__tests__/AccountDetails.test.tsx
@@ -8,21 +8,21 @@ describe("<AccountDetails />", () => {
       renderer.create(
         <AccountDetails
           account={{
-            id: "account",
-            amount: "14567898765434567876543",
-            locked: "0",
-            storageUsage: 876,
-            storagePaidAt: 1,
+            accountId: "megan.near",
             inTransactionsCount: 10,
             outTransactionsCount: 7,
             createdAtBlockTimestamp: Number(new Date(2019, 1, 1)),
             createdByTransactionHash:
               "EVvWW1S9BFaEjY1JBNSdstb7ZTtTFjQ6cygkbw1KY4tL",
             accountIndex: 1234567890,
-            storageAmountPerByte: "1000000000000000000000",
-            lockupAccountId:
-              "c3e5988357bc5ffe511fff286e03d05505a76bc1.lockup.near",
-            lockupAmount: "5000000000000000000000000",
+            stakedBalance: "0",
+            nonStakedBalance: "654345665432345678765",
+            minimumBalance: "87600000000000",
+            availableBalance: "43454321345678765432345678",
+            totalBalance: "9876545678765432134567876543",
+            storageUsage: "876",
+            lockupTotalBalance: "765654565324543",
+            lockupAccountId: "gjturigjgkjnfidsjffjsa.lockup.near",
           }}
         />
       )
