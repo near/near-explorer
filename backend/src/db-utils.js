caa2e24875d185c0e835e7927c76f0ec4862061c
Yifang Ma <yifang@nearprotocol.com>
2020-11-25 09:37:44 -0800

    fix the query

--- backend/src/db-utils.js
+++ backend/src/db-utils.js
@@ -175,15 +175,17 @@ const queryDashboardTxInfo = async (options) => {
   async function queryTransactionCountArray({ dataSource }) {
     let query;
     if (dataSource === DS_INDEXER_BACKEND) {
-      query = `SELECT date_trunc('day', to_timestamp(DIV(block_timestamp, 1000*1000*1000))) as date, count(1) as total
+      query = `SELECT date_trunc('day', to_timestamp(DIV(block_timestamp, 1000*1000*1000))) as date, count(transaction_hash) as total
                 FROM transactions
                 WHERE block_timestamp > (cast(EXTRACT(EPOCH FROM NOW()) - 60 * 60 * 24 * 14 as bigint) * 1000 * 1000 * 1000)
-                GROUP BY 1`;
+                GROUP BY date 
+                order by date`;
     } else {
-      query = `SELECT strftime('%Y-%m-%d',block_timestamp/1000,'unixepoch') as date, count(1) as total
+      query = `SELECT strftime('%Y-%m-%d',block_timestamp/1000,'unixepoch') as date, count(hash) as total
                 FROM transactions
                 WHERE (block_timestamp/1000) > (strftime('%s','now') - 60 * 60 * 24 * 15)
-                GROUP BY 1`;
+                GROUP BY date
+                ORDER BY date`;
     }
     return await queryRows([query], { dataSource });
   }
