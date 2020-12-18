f4f42974bdadf114687f7b91aff4896ab2f353dd
Yifang Ma <yifang@nearprotocol.com>
2020-12-17 17:55:00 -0800

    finish the plot

--- frontend/src/components/utils/TransactionCharts.jsx
+++ frontend/src/components/utils/TransactionCharts.jsx
@@ -1,24 +1,86 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactEcharts from "echarts-for-react";
 import echarts from "echarts";
+import { DatabaseContext } from "../../context/DatabaseProvider";
 
-export default class extends React.Component {
-  render() {
-    const option = {
-      xAxis: {
-        type: "category",
-        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+export default () => {
+  const value = useContext(DatabaseContext);
+  const transactionCountArray = value.transactionCountArray;
+  const date = transactionCountArray.map((t) => t.date);
+  const count = transactionCountArray.map((t) => t.total);
+  const getOption = () => {
+    return {
+      title: {
+        text: "14 Day History ",
       },
-      yAxis: {
-        type: "value",
+      tooltip: {
+        trigger: "axis",
       },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
+        containLabel: true,
+        backgroundColor: "#F9F9F9",
+        show: true,
+        color: "white",
+      },
+      xAxis: [
+        {
+          type: "category",
+          boundaryGap: false,
+          data: date,
+        },
+      ],
+      yAxis: [
+        {
+          type: "value",
+          splitLine: {
+            lineStyle: {
+              color: "white",
+            },
+          },
+        },
+      ],
       series: [
         {
-          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          name: "Txns",
           type: "line",
+          smooth: true,
+          lineStyle: {
+            color: "#00C1DE",
+            width: 2,
+          },
+          symbol: "circle",
+          itemStyle: {
+            color: "#25272A",
+          },
+          areaStyle: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              {
+                offset: 0,
+                color: "rgb(0, 193, 222)",
+              },
+              {
+                offset: 1,
+                color: "rgb(197, 247, 255)",
+              },
+            ]),
+          },
+          data: count,
         },
       ],
     };
-    return <ReactEcharts option={option} />;
-  }
-}
+  };
+  return (
+    <div className="examples">
+      <div className="parent">
+        <ReactEcharts
+          option={getOption()}
+          style={{ height: "176px", width: "650px" }}
+          className="react_for_echarts"
+        />
+      </div>
+    </div>
+  );
+};
