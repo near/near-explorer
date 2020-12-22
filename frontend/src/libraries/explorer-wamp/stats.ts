82cfa817144872a6eddd9f7c0aba2af4939266f9
Yifang Ma <yifang@nearprotocol.com>
2020-12-22 15:26:03 -0800

    fix test

--- frontend/src/libraries/explorer-wamp/stats.ts
+++ frontend/src/libraries/explorer-wamp/stats.ts
@@ -21,19 +21,19 @@ export interface NewContractsByDate {
 }
 
 export default class StatsApi extends ExplorerApi {
-  async transactionsByDate(): Promise<TransactionsByDate> {
-    return await this.call<TransactionsByDate>("transactions-by-date");
+  async transactionsByDate(): Promise<TransactionsByDate[]> {
+    return await this.call<TransactionsByDate[]>("transactions-by-date");
   }
 
-  async teragasUsedByDate(): Promise<TeragasUsedByDate> {
-    return await this.call<TeragasUsedByDate>("teragas-used-by-date");
+  async teragasUsedByDate(): Promise<TeragasUsedByDate[]> {
+    return await this.call<TeragasUsedByDate[]>("teragas-used-by-date");
   }
 
-  async newAccountsByDate(): Promise<NewAccountsByDate> {
-    return await this.call<NewAccountsByDate>("new-accounts-by-date");
+  async newAccountsByDate(): Promise<NewAccountsByDate[]> {
+    return await this.call<NewAccountsByDate[]>("new-accounts-by-date");
   }
 
-  async newContractsByDate(): Promise<NewContractsByDate> {
-    return await this.call<NewContractsByDate>("new-contracts-by-date");
+  async newContractsByDate(): Promise<NewContractsByDate[]> {
+    return await this.call<NewContractsByDate[]>("new-contracts-by-date");
   }
 }
