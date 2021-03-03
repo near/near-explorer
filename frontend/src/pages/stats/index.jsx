019fc3d0fdb26a1e7643f169636b0c174add9507
Yifang Ma <yifang@nearprotocol.com>
2021-03-03 09:48:59 -0800

    delete

--- frontend/src/pages/stats/index.jsx
+++ frontend/src/pages/stats/index.jsx
@@ -29,7 +29,6 @@ export default class extends React.PureComponent {
           <title>NEAR Explorer | Stats</title>
         </Head>
         <Content title={<h1>Stats</h1>}>
-          <Sidebar />
           <div id="transactionsByDate">
             <TransactionsByDate chartStyle={chartStyle} />
           </div>
@@ -78,71 +77,3 @@ export default class extends React.PureComponent {
     );
   }
 }
-
-const SideBarContent = () => {
-  return (
-    <Nav className="stats-sidebar">
-      <Nav.Link href="#transactionsByDate">Transactions Amount</Nav.Link>
-      <Nav.Link href="#gasUsedByDate">Used Tera Gas</Nav.Link>
-      <Nav.Link href="#newAccountsByDate">New Accounts</Nav.Link>
-      <Nav.Link href="#newContractsByDate">New Contracts</Nav.Link>
-      <Nav.Link href="#activeAccountsBydate">Active Accounts</Nav.Link>
-      <Nav.Link href="#activeContractsByDate">Active Contracts</Nav.Link>
-      <Nav.Link href="#activeAccountsList">Top 10 of Active Accounts</Nav.Link>
-      <Nav.Link href="#activeContractsList">
-        Top 10 of Active Contracts
-      </Nav.Link>
-      <Nav.Link href="#validators">Staking Pool</Nav.Link>
-      <style jsx global>{`
-        .stats-sidebar {
-          width: 100%;
-        }
-        .stats-sidebar .nav-link {
-          color: #042772;
-          display: block;
-          width: 100%;
-        }
-      `}</style>
-    </Nav>
-  );
-};
-
-const MobileSideBar = () => (
-  <Dropdown>
-    <Dropdown.Toggle variant="success" id="dropdown-basic">
-      <img className="sidebar-icon" src={"/static/images/icon-sidebar.svg"} />
-    </Dropdown.Toggle>
-
-    <Dropdown.Menu>
-      <SideBarContent />
-    </Dropdown.Menu>
-
-    <style jsx global>{`
-      .sidebar-icon {
-        height: 16px;
-        width: 16px;
-      }
-    `}</style>
-  </Dropdown>
-);
-
-const Sidebar = () => (
-  <Row className="sidebar">
-    <Col lg="12" className="d-none d-lg-block">
-      <SideBarContent />
-    </Col>
-    <Col xs="12" className="d-lg-none text-left">
-      <MobileSideBar />
-    </Col>
-    <style jsx global>{`
-      .sidebar {
-        position: fixed;
-        left: 5%;
-        z-index: 10;
-        top: 50%;
-        width: 80%;
-        max-width: 300px;
-      }
-    `}</style>
-  </Row>
-);
