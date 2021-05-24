4493524cf0411f21e6a501a480812e3a6590ab4c
Vlad Frolov <frolvlad@gmail.com>
2021-05-24 20:37:59 +0300

    fix(frontend): fixed the DetailsApi.getLatestCirculatingSupply implementation

--- frontend/src/libraries/explorer-wamp/details.ts
+++ frontend/src/libraries/explorer-wamp/details.ts
@@ -1,12 +1,19 @@
 import { ExplorerApi } from ".";
 
 export interface CirculatingSupply {
-  block_height: number;
-  circulating_supply_in_yoctonear: string;
+  blockHeight: number;
+  circulatingSupplyInYoctonear: BN;
 }
 
 export default class DetailsApi extends ExplorerApi {
-  async calculateCirculatingSupply(): Promise<CirculatingSupply> {
-    return await this.call<CirculatingSupply>("calculate-circulating-supply");
+  async getLatestCirculatingSupply(): Promise<CirculatingSupply> {
+    const {
+      block_height,
+      circulating_supply_in_yoctonear,
+    } = await this.call<CirculatingSupply>("get-latest-circulating-supply");
+    return {
+      blockHeight: block_height,
+      circulatingSupplyInYoctonear: new BN(circulating_supply_in_yoctonear),
+    };
   }
 }
