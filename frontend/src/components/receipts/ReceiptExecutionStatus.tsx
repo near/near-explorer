71ac07e5f0e78b6bb5ac34027f7266c0101ca3c7
Vlad Frolov <frolvlad@gmail.com>
2021-06-08 11:07:14 +0300

    refactor(frontend): added a comment stating why it is ok to have 'Succeeded' status for the Receipt SucceessReceiptId variant

--- frontend/src/components/receipts/ReceiptExecutionStatus.tsx
+++ frontend/src/components/receipts/ReceiptExecutionStatus.tsx
@@ -4,6 +4,9 @@ const EXECUTION_RECEIPT_STATUSES: Record<ReceiptExecutionStatus, string> = {
   Unknown: "Unknown",
   Failure: "Failed",
   SuccessValue: "Succeeded",
+  // The transaction execution status will be recursively deferred to the status of the receipt id,
+  // but given we only care about the details when we display the whole transaction execution plan,
+  // it is fine to just report the receipt status as "Succeeded"
   SuccessReceiptId: "Succeeded",
 };
 
