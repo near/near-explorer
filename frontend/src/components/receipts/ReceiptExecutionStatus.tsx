6bc6fdd7073bd66a1bf1bad5bad13ca27db8e9ab
qiao <qxwxintu@gmail.com>
2021-07-24 13:53:31 +0800

    fmt code

--- frontend/src/components/receipts/ReceiptExecutionStatus.tsx
+++ frontend/src/components/receipts/ReceiptExecutionStatus.tsx
@@ -1,21 +1,32 @@
 import { ReceiptExecutionStatus } from "../../libraries/explorer-wamp/receipts";
 import { Translate, TranslateFunction } from "react-localize-redux";
 
-const getExecutionReceiptStatuses = (status: ReceiptExecutionStatus, translate: TranslateFunction): String => {
+const getExecutionReceiptStatuses = (
+  status: ReceiptExecutionStatus,
+  translate: TranslateFunction
+): String => {
   switch (status) {
     case "Unknown":
-      return translate("component.receipts.ReceiptExecutionStatus.unknown").toString()
+      return translate(
+        "component.receipts.ReceiptExecutionStatus.unknown"
+      ).toString();
     case "Failure":
-      return translate("component.receipts.ReceiptExecutionStatus.failure").toString()
+      return translate(
+        "component.receipts.ReceiptExecutionStatus.failure"
+      ).toString();
     case "SuccessValue":
-      return translate("component.receipts.ReceiptExecutionStatus.success_value").toString()
+      return translate(
+        "component.receipts.ReceiptExecutionStatus.success_value"
+      ).toString();
     // The transaction execution status will be recursively deferred to the status of the receipt id,
     // but given we only care about the details when we display the whole transaction execution plan,
     // it is fine to just report the receipt status as "Succeeded"
     case "SuccessReceiptId":
-      return translate("component.receipts.ReceiptExecutionStatus.success_receiptId").toString()
+      return translate(
+        "component.receipts.ReceiptExecutionStatus.success_receiptId"
+      ).toString();
   }
-  return ""
+  return "";
 };
 
 export interface Props {
@@ -23,11 +34,13 @@ export interface Props {
 }
 
 const ReceiptExecutionStatusComponent = (prop: Props) => {
-  return <Translate>
-    {({ translate }) => (
-      <>{getExecutionReceiptStatuses(prop.status, translate)}</>
-    )}
-  </Translate>
+  return (
+    <Translate>
+      {({ translate }) => (
+        <>{getExecutionReceiptStatuses(prop.status, translate)}</>
+      )}
+    </Translate>
+  );
 };
 
 export default ReceiptExecutionStatusComponent;
