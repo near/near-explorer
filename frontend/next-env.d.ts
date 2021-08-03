6dbb5e2be6ab25a1a62f29921266b3b00b53e281
shelegdmitriy <sheleg.dmitriy@gmail.com>
2021-08-03 15:46:43 +0300

    Fix failing test

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,3 +1,13 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
+
+declare module "*.svg" {
+  const content: React.FC;
+  export default content;
+}
+
+declare module "react-text-collapse";
+declare module "echarts-for-react";
+
+declare module "*"; // for datamaps npm
