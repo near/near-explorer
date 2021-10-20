4922bc1f844b092be85ba6ce5aaea3be1f785a4d
shelegdmitriy <sheleg.dmitriy@gmail.com>
2021-10-20 10:49:01 +0300

    Add localization to component

--- frontend/src/components/utils/StorageSize.tsx
+++ frontend/src/components/utils/StorageSize.tsx
@@ -1,42 +1,64 @@
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Translate, TranslateFunction } from "react-localize-redux";
 import { formatWithCommas } from "../utils/Balance";
 
 interface Props {
   value: number;
 }
 const StorageSize = ({ value }: Props) => {
-  let showStorage;
-  const byte = "B";
-  const kilo = 10 ** 3;
+  const formatStoreSize = (
+    value: number,
+    translate: TranslateFunction
+  ): string => {
+    let showStorage = value.toString();
+    const kilo = 10 ** 3;
 
-  const units = [
-    { value: kilo, symbol: "kB" },
-    { value: kilo ** 2, symbol: "MB" },
-    { value: kilo ** 3, symbol: "GB" },
-  ];
+    const units = [
+      {
+        value: kilo,
+        symbol: translate("utils.StorageSize.kilo_bytes").toString(),
+      },
+      {
+        value: kilo ** 2,
+        symbol: translate("utils.StorageSize.mega_bytes").toString(),
+      },
+      {
+        value: kilo ** 3,
+        symbol: translate("utils.StorageSize.giga_bytes").toString(),
+      },
+    ];
 
-  if (value >= kilo) {
-    for (let i = 0; i < units.length; i++) {
-      if (value >= units[i].value) {
-        const roundValue = Math.round(value / units[i].value);
-        showStorage = `${roundValue} ${units[i].symbol}`;
+    if (value >= kilo) {
+      for (let i = 0; i < units.length; i++) {
+        if (value >= units[i].value) {
+          const roundValue = Math.round(value / units[i].value);
+          showStorage = `${roundValue} ${units[i].symbol}`;
+        }
       }
+    } else {
+      showStorage = `${value} ${translate("utils.StorageSize.bytes")}`;
     }
-  } else {
-    showStorage = `${value} ${byte}`;
-  }
+
+    return showStorage;
+  };
 
   return (
-    <OverlayTrigger
-      placement={"bottom"}
-      overlay={
-        <Tooltip id={`storage_size_${value}`}>
-          {`${formatWithCommas(value)} ${byte}`}
-        </Tooltip>
-      }
-    >
-      <span>{showStorage}</span>
-    </OverlayTrigger>
+    <Translate>
+      {({ translate }) => (
+        <OverlayTrigger
+          placement={"bottom"}
+          overlay={
+            <Tooltip id={`storage_size_${value}`}>
+              {`${formatWithCommas(value)} ${translate(
+                "utils.StorageSize.bytes"
+              )}`}
+            </Tooltip>
+          }
+        >
+          <span>{formatStoreSize(value, translate)}</span>
+        </OverlayTrigger>
+      )}
+    </Translate>
   );
 };
 
