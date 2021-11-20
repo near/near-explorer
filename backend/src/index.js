6c181e20f268b485699ebc7d68e2dec24c985414
shelegdmitriy <34593263+shelegdmitriy@users.noreply.github.com>
2021-11-20 09:05:19 +0200

    refactor(backend): Catch error when connect to telemetry db failed (#778)

--- backend/src/index.js
+++ backend/src/index.js
@@ -140,7 +140,11 @@ function startStatsAggregation() {
 async function main() {
   console.log("Starting Explorer backend...");
 
-  await models.sequelizeTelemetryBackend.sync();
+  try {
+    await models.sequelizeTelemetryBackend.sync();
+  } catch (error) {
+    console.warn("Initialization of Telemetry database failed, but Explorer backend can operate without it. Explorer backend won't be able to handle telemetry reports. More details about the error:", error);
+  }
 
   const wamp = setupWamp();
   console.log("Starting WAMP worker...");
