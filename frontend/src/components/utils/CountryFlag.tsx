3f0036e8e6858967c9afbb1be88dfd82b283e998
Vlad Frolov <frolvlad@gmail.com>
2021-12-07 16:03:03 +0100

    fix(frontend): fixed Validators page where unknown countryCode could crash the whole page (#794)

--- frontend/src/components/utils/CountryFlag.tsx
+++ frontend/src/components/utils/CountryFlag.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactCountryFlag from "react-country-flag";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { countries } from "country-data";
 
 interface Props {
   countryCode?: string;
+  id: string;
+  country?: string;
 }
 
 const DefaultCountryFlag = () => (
   <div
     style={{
+      display: "inline-block",
       width: "22px",
       lineHeight: "16px",
       backgroundColor: "#f0f0f1",
@@ -20,22 +25,45 @@ const DefaultCountryFlag = () => (
   </div>
 );
 
-const CountryFlag = (props: Props) => (
-  <>
-    {props.countryCode ? (
-      <ReactCountryFlag
-        {...props}
-        svg
-        countryCode={props.countryCode}
-        style={{
-          width: "22px",
-          lineHeight: "14px",
-        }}
-      />
-    ) : (
-      <DefaultCountryFlag />
-    )}
-  </>
-);
+const CountryFlag = (props: Props) => {
+  // sometimes in 'country' can be actually countryCode
+  let countryCode: string | undefined = props.countryCode?.toUpperCase();
+  if (!(countryCode! in countries)) {
+    const countryAsCountryCode = props.country?.toUpperCase();
+    if (countryAsCountryCode! in countries) {
+      countryCode = countryAsCountryCode;
+    }
+  }
+  const countryLabel: string | undefined =
+    countries[countryCode!]?.name ?? props.country;
+
+  return (
+    <>
+      {countryLabel ? (
+        <OverlayTrigger
+          overlay={<Tooltip id={props.id}>{countryLabel}</Tooltip>}
+        >
+          {countryCode ? (
+            <ReactCountryFlag
+              {...props}
+              svg
+              countryCode={countryCode}
+              style={{
+                width: "22px",
+                lineHeight: "14px",
+              }}
+            />
+          ) : (
+            <span>
+              <DefaultCountryFlag />
+            </span>
+          )}
+        </OverlayTrigger>
+      ) : (
+        <DefaultCountryFlag />
+      )}
+    </>
+  );
+};
 
 export default CountryFlag;
