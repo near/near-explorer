e7537d13576e5715dfa102c1607cb31c17e21482
Alexey Immoreev <lexx92@mail.ru>
2021-12-14 15:11:04 +0300

    Upgrade next.js 11 -> 12

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,7 +2,13 @@ module.exports = {
   setupFiles: ["<rootDir>/jest.setup.js", "jest-date-mock"],
   testRegex: "(\\.|/)test\\.[jt]sx?$",
   transform: {
-    "^.+\\.([jt]sx?)$": "babel-jest",
+    "^.+\\.([jt]sx?)$": [
+      "babel-jest",
+      {
+        plugins: ["inline-react-svg"],
+        presets: ["next/babel"],
+      },
+    ],
   },
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
