2a1dea85db6f79e10b3eb5d2adba8ad846bba124
Alexey Immoreev <lexx92@mail.ru>
2022-01-25 16:47:05 +0300

    Fix: date language in SSR

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -11,7 +11,11 @@ import Footer from "../components/utils/Footer";
 import { NetworkContext } from "../context/NetworkContext";
 import DatabaseProvider from "../context/DatabaseProvider";
 
-import { getLanguage, LANGUAGE_COOKIE } from "../libraries/language";
+import {
+  getLanguage,
+  LANGUAGE_COOKIE,
+  setMomentLanguage,
+} from "../libraries/language";
 import { useAnalyticsInit } from "../hooks/analytics/use-analytics-init";
 import { initializeI18n, Language, LANGUAGES } from "../libraries/i18n";
 import { NextComponentType } from "next";
@@ -158,6 +162,7 @@ App.getInitialProps = async (appContext) => {
       language,
     };
     setI18n(await initializeI18n(language));
+    setMomentLanguage(language);
     appContext.ctx.res!.setHeader(
       "set-cookie",
       `${LANGUAGE_COOKIE}=${language}; Max-Age=${YEAR / 1000}`
