eab6fa1e9592ddbccbbe037bd10f1b8814a4d94e
Alexey Immoreev <lexx92@mail.ru>
2022-04-20 21:26:15 +0400

    Refactor: validator metadata row

--- frontend/src/components/nodes/ValidatorMetadataRow.tsx
+++ frontend/src/components/nodes/ValidatorMetadataRow.tsx
@@ -16,6 +16,28 @@ const ValidatorNodesText = styled(Col, {
   color: "#3f4045",
 });
 
+type ElementProps = {
+  header: React.ReactNode;
+};
+
+const ValidatorMetadataElement: React.FC<ElementProps> = React.memo(
+  ({ children, header }) => {
+    if (!children) {
+      return null;
+    }
+    return (
+      <ValidatorNodesContentCell xs="auto">
+        <Row noGutters>
+          <ValidatorNodesDetailsTitle>{header}</ValidatorNodesDetailsTitle>
+        </Row>
+        <Row noGutters>
+          <ValidatorNodesText>{children}</ValidatorNodesText>
+        </Row>
+      </ValidatorNodesContentCell>
+    );
+  }
+);
+
 interface Props {
   poolDetails?: PoolDetails;
 }
@@ -48,98 +70,61 @@ const ValidatorMetadataRow: React.FC<Props> = React.memo(({ poolDetails }) => {
 
   return (
     <ValidatorNodesContentRow noGutters>
-      {poolDetails.url && (
-        <ValidatorNodesContentCell xs="auto">
-          <Row noGutters>
-            <ValidatorNodesDetailsTitle>
-              {t("component.nodes.ValidatorMetadataRow.pool_info.website")}
-            </ValidatorNodesDetailsTitle>
-          </Row>
-          <Row noGutters>
-            <ValidatorNodesText>
-              <a
-                href={
-                  poolDetails.url.startsWith("http")
-                    ? poolDetails.url
-                    : `http://${poolDetails.url}`
-                }
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                {poolDetails.url}
-              </a>
-            </ValidatorNodesText>
-          </Row>
-        </ValidatorNodesContentCell>
-      )}
-      {poolDetails.email && (
-        <ValidatorNodesContentCell xs="auto">
-          <Row noGutters>
-            <ValidatorNodesDetailsTitle>
-              {t("component.nodes.ValidatorMetadataRow.pool_info.email")}
-            </ValidatorNodesDetailsTitle>
-          </Row>
-          <Row noGutters>
-            <ValidatorNodesText>
-              <a href={`mailto:${poolDetails.email}`}>{poolDetails.email}</a>
-            </ValidatorNodesText>
-          </Row>
-        </ValidatorNodesContentCell>
-      )}
-      {poolDetails.twitter && (
-        <ValidatorNodesContentCell xs="auto">
-          <Row noGutters>
-            <ValidatorNodesDetailsTitle>
-              {t("component.nodes.ValidatorMetadataRow.pool_info.twitter")}
-            </ValidatorNodesDetailsTitle>
-          </Row>
-          <Row noGutters>
-            <ValidatorNodesText>
-              <a
-                href={`https://twitter.com/${poolDetails.twitter}`}
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                {poolDetails.twitter}
-              </a>
-            </ValidatorNodesText>
-          </Row>
-        </ValidatorNodesContentCell>
-      )}
-      {poolDetails.discord && (
-        <ValidatorNodesContentCell xs="auto">
-          <Row noGutters>
-            <ValidatorNodesDetailsTitle>
-              {t("component.nodes.ValidatorMetadataRow.pool_info.discord")}
-            </ValidatorNodesDetailsTitle>
-          </Row>
-          <Row noGutters>
-            <ValidatorNodesText>
-              <a
-                href={poolDetails.discord}
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                {poolDetails.discord}
-              </a>
-            </ValidatorNodesText>
-          </Row>
-        </ValidatorNodesContentCell>
-      )}
-      {poolDetails.description && (
-        <ValidatorNodesContentCell>
-          <Row noGutters>
-            <ValidatorNodesDetailsTitle>
-              {t("component.nodes.ValidatorMetadataRow.pool_info.description")}
-            </ValidatorNodesDetailsTitle>
-          </Row>
-          <Row noGutters>
-            <ValidatorNodesText>
-              <small>{poolDetails.description}</small>
-            </ValidatorNodesText>
-          </Row>
-        </ValidatorNodesContentCell>
-      )}
+      <ValidatorMetadataElement
+        header={t("component.nodes.ValidatorMetadataRow.pool_info.website")}
+      >
+        {poolDetails.url && (
+          <a
+            href={
+              poolDetails.url.startsWith("http")
+                ? poolDetails.url
+                : `http://${poolDetails.url}`
+            }
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            {poolDetails.url}
+          </a>
+        )}
+      </ValidatorMetadataElement>
+      <ValidatorMetadataElement
+        header={t("component.nodes.ValidatorMetadataRow.pool_info.email")}
+      >
+        {poolDetails.email && (
+          <a href={`mailto:${poolDetails.email}`}>{poolDetails.email}</a>
+        )}
+      </ValidatorMetadataElement>
+      <ValidatorMetadataElement
+        header={t("component.nodes.ValidatorMetadataRow.pool_info.twitter")}
+      >
+        {poolDetails.twitter && (
+          <a
+            href={`https://twitter.com/${poolDetails.twitter}`}
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            {poolDetails.twitter}
+          </a>
+        )}
+      </ValidatorMetadataElement>
+      <ValidatorMetadataElement
+        header={t("component.nodes.ValidatorMetadataRow.pool_info.discord")}
+      >
+        {poolDetails.discord && (
+          <a
+            href={poolDetails.discord}
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            {poolDetails.discord}
+          </a>
+        )}
+      </ValidatorMetadataElement>
+      <ValidatorMetadataElement
+        header={t("component.nodes.ValidatorMetadataRow.pool_info.description")}
+      >
+        {poolDetails.description && <small>{poolDetails.description}</small>}
+      </ValidatorMetadataElement>
     </ValidatorNodesContentRow>
   );
 });
