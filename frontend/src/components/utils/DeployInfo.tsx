c3e81622a985220ea191a7b6088cf1642f4173a1
Alexey Immoreev <lexx92@mail.ru>
2022-05-01 02:39:52 +0400

    Feature: deploy info

---
+++ frontend/src/components/utils/DeployInfo.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { useFetch } from "../../hooks/use-fetch";
+import { styled } from "../../libraries/styles";
+import { DeployInfo as DeployInfoProps } from "../../types/procedures";
+
+const Wrapper = styled("div", {
+  position: "fixed",
+  bottom: 4,
+  right: 4,
+  padding: "0 4px",
+  borderRadius: 6,
+  cursor: "pointer",
+
+  variants: {
+    expanded: {
+      true: {
+        border: "1px solid gray",
+        background: "white",
+      },
+      false: {
+        color: "green",
+      },
+    },
+  },
+});
+
+const Section = styled("div", {
+  margin: 4,
+  padding: "inherit",
+});
+
+const Title = styled("div", {
+  fontWeight: 500,
+  marginTop: 12,
+});
+
+type Props = {
+  client: DeployInfoProps;
+};
+
+export const DeployInfo: React.FC<Props> = ({ client }) => {
+  const [expanded, setExpanded] = React.useState(false);
+  const switchExpanded = React.useCallback(
+    () => setExpanded((expanded) => !expanded),
+    [setExpanded]
+  );
+  const server = useFetch("deploy-info", [], !expanded);
+  let content: React.ReactNode = "◍";
+  if (expanded) {
+    content = (
+      <>
+        <Section>
+          <Title>Client:</Title>
+          <div>Branch: {client.branch}</div>
+          <div>Commit: {client.commit}</div>
+          <div>Instance id: {client.instanceId}</div>
+          <div>Service id: {client.serviceId}</div>
+          <div>Service name: {client.serviceName}</div>
+        </Section>
+        <Section>
+          <Title>Server:</Title>
+          {server ? (
+            <>
+              <div>Branch: {server.branch}</div>
+              <div>Commit: {server.commit}</div>
+              <div>Instance id: {server.instanceId}</div>
+              <div>Service id: {server.serviceId}</div>
+              <div>Service name: {server.serviceName}</div>
+            </>
+          ) : (
+            <div>loading..</div>
+          )}
+        </Section>
+      </>
+    );
+  }
+  return (
+    <Wrapper onClick={switchExpanded} expanded={expanded}>
+      {content}
+    </Wrapper>
+  );
+};
