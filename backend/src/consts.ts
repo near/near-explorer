5b6532b41a4a7af30a09f7d20b1857d776cd9f83
Alexey Immoreev <lexx92@mail.ru>
2022-05-11 03:55:28 +0400

    Fix: rm partner list comment

--- backend/src/consts.ts
+++ backend/src/consts.ts
@@ -7,8 +7,3 @@ export enum DataSource {
 export const SECOND = 1000;
 export const MINUTE = 60 * SECOND;
 export const HOUR = 60 * MINUTE;
-
-// parter list is from the query
-// select distinct receiver_account_id
-// from transactions
-// where receiver_account_id like any (array['%zest%', '%berryclub%', '%paras%', '%bananaswap%', '%tessab%'])
