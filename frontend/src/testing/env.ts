b1bc6b3245c7579bbb4ee2e7bb05112bf1af8011
Alexey Immoreev <lexx92@mail.ru>
2022-05-20 02:35:12 +0400

    Fix: autobahn tests

--- frontend/src/testing/env.ts
+++ frontend/src/testing/env.ts
@@ -1,6 +1,7 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import JSDomEnvironment from "jest-environment-jsdom";
+import { TextEncoder, TextDecoder } from "util";
 
 export default class extends JSDomEnvironment {
   async setup() {
@@ -12,5 +13,10 @@ export default class extends JSDomEnvironment {
     });
     // Will use this instance in renderElement
     this.global.i18nInstance = i18nInstance;
+    this.global.TextEncoder = TextEncoder;
+    // Types between DOM TextDecoder and node.js TextDecoder don't match
+    // But we need to have some kind of polyfill for autobahn to not throw errors
+    // @ts-ignore
+    this.global.TextDecoder = TextDecoder;
   }
 }
