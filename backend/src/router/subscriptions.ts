edeca3d110bce666329758df9b7832218041bfba
Alexey Immoreev <lexx92@mail.ru>
2022-06-16 19:27:12 +0400

    Feature: generic subscriptions helper

--- backend/src/router/subscriptions.ts
+++ backend/src/router/subscriptions.ts
@@ -1,128 +1,87 @@
 import * as trpc from "@trpc/server";
-import { ProcedureResolver } from "@trpc/server/dist/declarations/src/internals/procedure";
-import { Subscription } from "@trpc/server";
 
 import { Context } from "../context";
 import { SubscriptionEventMap } from "./types";
 import { SSR_TIMEOUT, wait } from "../common";
+import {
+  AnyRouter,
+  CreateProcedureSubscription,
+  RouterWithSubscriptionsAndQueries,
+} from "../types";
 
-const getSubscriptionResolve =
-  <S extends keyof SubscriptionEventMap>(
-    path: S
-  ): ProcedureResolver<
-    Context,
-    undefined,
-    Subscription<Parameters<SubscriptionEventMap[S]>[0]>
-  > =>
-  async ({ ctx }) =>
-    new trpc.Subscription<Parameters<SubscriptionEventMap[S]>[0]>((emit) => {
-      const onData = emit.data as SubscriptionEventMap[S];
-      if (ctx.subscriptionsCache[path]) {
-        onData(ctx.subscriptionsCache[path]!);
-      }
-      ctx.subscriptionsEventEmitter.on(path, onData);
-      return () => void ctx.subscriptionsEventEmitter.off(path, onData);
-    });
+const withTopics = <InitialRouter extends AnyRouter<Context>>(
+  router: InitialRouter,
+  topicsWithFilterFns: {
+    [T in keyof SubscriptionEventMap]: undefined;
+  }
+): RouterWithSubscriptionsAndQueries<
+  InitialRouter,
+  {
+    [SpecificTopic in keyof typeof topicsWithFilterFns]: CreateProcedureSubscription<
+      InitialRouter,
+      Parameters<SubscriptionEventMap[SpecificTopic]>[0]
+    >;
+  }
+> => {
+  type TopicsFns = typeof topicsWithFilterFns;
+  return (
+    Object.entries(topicsWithFilterFns) as [
+      keyof TopicsFns,
+      TopicsFns[keyof TopicsFns]
+    ][]
+  ).reduce((router, [topic, filterFn]) => {
+    return router
+      .subscription(topic, {
+        resolve: ({ ctx }) => {
+          return new trpc.Subscription<
+            Parameters<SubscriptionEventMap[typeof topic]>[0]
+          >((emit) => {
+            const onData = (
+              data: Parameters<SubscriptionEventMap[typeof topic]>[0]
+            ) => {
+              emit.data(data);
+            };
+            if (ctx.subscriptionsCache[topic]) {
+              onData(ctx.subscriptionsCache[topic]!);
+            }
+            ctx.subscriptionsEventEmitter.on(topic, onData);
+            return () => void ctx.subscriptionsEventEmitter.off(topic, onData);
+          });
+        },
+      })
+      .query(topic, {
+        resolve: async ({ ctx }) => {
+          if (!ctx.subscriptionsCache[topic]) {
+            return new Promise(async (resolve) => {
+              const onData = (
+                data: Parameters<SubscriptionEventMap[typeof topic]>[0]
+              ) => {
+                ctx.subscriptionsEventEmitter.off(topic, onData);
+                resolve(data);
+              };
+              ctx.subscriptionsEventEmitter.on(topic, onData);
+              await wait(SSR_TIMEOUT);
+              ctx.subscriptionsEventEmitter.off(topic, onData);
+            });
+          }
+          const cachedData = ctx.subscriptionsCache[topic] as Parameters<
+            SubscriptionEventMap[typeof topic]
+          >[0] &
+            Parameters<NonNullable<typeof filterFn>>[0];
+          return cachedData;
+        },
+      }) as any;
+  }, router) as any;
+};
 
-const getQueryResolve =
-  <S extends keyof SubscriptionEventMap>(
-    path: S
-  ): ProcedureResolver<
-    Context,
-    undefined,
-    Parameters<SubscriptionEventMap[S]>[0] | undefined
-  > =>
-  async ({ ctx }) => {
-    if (!ctx.subscriptionsCache[path]) {
-      return new Promise(async (resolve) => {
-        const onData = ((data) => {
-          ctx.subscriptionsEventEmitter.off(path, onData);
-          resolve(data);
-        }) as SubscriptionEventMap[S];
-        ctx.subscriptionsEventEmitter.on(path, onData);
-        await wait(SSR_TIMEOUT);
-        ctx.subscriptionsEventEmitter.off(path, onData);
-      });
-    }
-    return ctx.subscriptionsCache[path];
-  };
-
-/*
-  TODO: write a function to add an ability to easily add subscriptions
-  Something like this:
-
-  const addSubscription = <S extends keyof SubscriptionEventMap>(
-    router: Router<..>,
-    path: S
-  ): Router<.. & OutputFromSubscription> => {
-    return router.query(path, {
-      resolve: () => { resolution of cache }
-    })
-    .subscription(path, {
-      resolve: () => { (un)subscription, emit lastValue on connect }
-    })
-  };
-
-  const router = ['chain-blocks-stats', ...].reduce((router, path) =>
-    addSubscription(router, path),
-    trpc.router<Context>()
-  );
-  export default router;
-*/
-
-export const router = trpc
-  .router<Context>()
-  .query("blockProductionSpeed", {
-    resolve: getQueryResolve("blockProductionSpeed"),
-  })
-  .subscription("blockProductionSpeed", {
-    resolve: getSubscriptionResolve("blockProductionSpeed"),
-  })
-  .query("latestBlock", {
-    resolve: getQueryResolve("latestBlock"),
-  })
-  .subscription("latestBlock", {
-    resolve: getSubscriptionResolve("latestBlock"),
-  })
-  .query("latestGasPrice", {
-    resolve: getQueryResolve("latestGasPrice"),
-  })
-  .subscription("latestGasPrice", {
-    resolve: getSubscriptionResolve("latestGasPrice"),
-  })
-  .query("validators", {
-    resolve: getQueryResolve("validators"),
-  })
-  .subscription("validators", {
-    resolve: getSubscriptionResolve("validators"),
-  })
-  .query("recentTransactionsCount", {
-    resolve: getQueryResolve("recentTransactionsCount"),
-  })
-  .subscription("recentTransactionsCount", {
-    resolve: getSubscriptionResolve("recentTransactionsCount"),
-  })
-  .query("onlineNodesCount", {
-    resolve: getQueryResolve("onlineNodesCount"),
-  })
-  .subscription("onlineNodesCount", {
-    resolve: getSubscriptionResolve("onlineNodesCount"),
-  })
-  .query("genesisConfig", {
-    resolve: getQueryResolve("genesisConfig"),
-  })
-  .subscription("genesisConfig", {
-    resolve: getSubscriptionResolve("genesisConfig"),
-  })
-  .query("tokensSupply", {
-    resolve: getQueryResolve("tokensSupply"),
-  })
-  .subscription("tokensSupply", {
-    resolve: getSubscriptionResolve("tokensSupply"),
-  })
-  .query("network-stats", {
-    resolve: getQueryResolve("network-stats"),
-  })
-  .subscription("network-stats", {
-    resolve: getSubscriptionResolve("network-stats"),
-  });
+export const router = withTopics(trpc.router<Context>(), {
+  blockProductionSpeed: undefined,
+  latestBlock: undefined,
+  latestGasPrice: undefined,
+  validators: undefined,
+  recentTransactionsCount: undefined,
+  onlineNodesCount: undefined,
+  genesisConfig: undefined,
+  tokensSupply: undefined,
+  "network-stats": undefined,
+});
