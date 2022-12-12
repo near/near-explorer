d712e6a9e01af513c59fd2e7e00a68fdd75375cc
Alexey Immoreev <lexx92@mail.ru>
2022-12-12 22:08:02 +0200

    Refactor: use cookies

--- frontend/src/hooks/use-language-context.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Language } from "../libraries/i18n";
-import { LANGUAGE_COOKIE } from "../libraries/language";
-import { useSetCookie } from "./use-set-cookie";
-
-export const useLanguageCookie = (language: Language) => {
-  const setCookie = useSetCookie(LANGUAGE_COOKIE);
-  React.useEffect(() => setCookie(language), [language]);
-};
