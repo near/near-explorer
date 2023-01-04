e6f839fb014cce7e93803bb65d665a1358a6992b
Alexey Immoreev <lexx92@mail.ru>
2023-01-04 13:38:20 +0300

    Refactor: optional string overlay (#1132)

---
+++ frontend/src/components/beta/common/StringConditionalOverlay.tsx
@@ -0,0 +1,28 @@
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { shortenString } from "../../../libraries/formatting";
+
+type Props = {
+  value: string;
+  tooltipId: string;
+  children: React.ReactElement;
+};
+
+const StringConditionalOverlay: React.FC<Props> = ({
+  value,
+  tooltipId,
+  children,
+}) => {
+  if (value.length === shortenString(value).length) {
+    return children;
+  }
+  return (
+    <OverlayTrigger
+      placement="top"
+      overlay={<Tooltip id={tooltipId}>{value}</Tooltip>}
+    >
+      {children}
+    </OverlayTrigger>
+  );
+};
+
+export default StringConditionalOverlay;
