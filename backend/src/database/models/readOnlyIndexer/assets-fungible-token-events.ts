65259c05f591018b8a1bdf7a1c87865c24da862f
Alexey Immoreev <lexx92@mail.ru>
2023-02-17 14:38:50 +0400

    Fix: DB types

--- backend/src/database/models/readOnlyIndexer/assets-fungible-token-events.ts
+++ backend/src/database/models/readOnlyIndexer/assets-fungible-token-events.ts
@@ -5,116 +5,78 @@ import FtEventKind from "./ft-event-kind";
 import { ReceiptsId } from "./receipts";
 
 export default interface AssetsFungibleTokenEvents {
-  /**
-   * Primary key. Index: assets__fungible_token_events_pkey
-   * Index: assets__fungible_token_events_unique
-   */
+  /** Primary key. Index: assets__fungible_token_events_pkey */
   emitted_for_receipt_id: ReceiptsId;
 
   /**
    * Index: assets__fungible_token_events_block_timestamp_idx
-   * Primary key. Index: assets__fungible_token_events_pkey
    * Index: assets__fungible_token_events_sorting_idx
    */
   emitted_at_block_timestamp: string;
 
-  /**
-   * Primary key. Index: assets__fungible_token_events_pkey
-   * Index: assets__fungible_token_events_sorting_idx
-   */
+  /** Index: assets__fungible_token_events_sorting_idx */
   emitted_in_shard_id: string;
 
   /**
    * Primary key. Index: assets__fungible_token_events_pkey
    * Index: assets__fungible_token_events_sorting_idx
-   * Index: assets__fungible_token_events_unique
    */
   emitted_index_of_event_entry_in_shard: number;
 
-  /**
-   * Index: assets__ft_contract_id_idx
-   * Primary key. Index: assets__fungible_token_events_pkey
-   */
+  /** Index: assets__ft_contract_id_idx */
   emitted_by_contract_account_id: string;
 
-  /** Primary key. Index: assets__fungible_token_events_pkey */
   amount: string;
 
-  /** Primary key. Index: assets__fungible_token_events_pkey */
   event_kind: FtEventKind;
 
-  /**
-   * Index: assets__fungible_token_events_old_owner_account_id_idx
-   * Primary key. Index: assets__fungible_token_events_pkey
-   */
+  /** Index: assets__fungible_token_events_old_owner_account_id_idx */
   token_old_owner_account_id: string;
 
-  /**
-   * Index: assets__fungible_token_events_new_owner_account_id_idx
-   * Primary key. Index: assets__fungible_token_events_pkey
-   */
+  /** Index: assets__fungible_token_events_new_owner_account_id_idx */
   token_new_owner_account_id: string;
 
-  /** Primary key. Index: assets__fungible_token_events_pkey */
   event_memo: string;
 }
 
 export interface AssetsFungibleTokenEventsInitializer {
-  /**
-   * Primary key. Index: assets__fungible_token_events_pkey
-   * Index: assets__fungible_token_events_unique
-   */
+  /** Primary key. Index: assets__fungible_token_events_pkey */
   emitted_for_receipt_id: ReceiptsId;
 
   /**
    * Index: assets__fungible_token_events_block_timestamp_idx
-   * Primary key. Index: assets__fungible_token_events_pkey
    * Index: assets__fungible_token_events_sorting_idx
    */
   emitted_at_block_timestamp: string;
 
-  /**
-   * Primary key. Index: assets__fungible_token_events_pkey
-   * Index: assets__fungible_token_events_sorting_idx
-   */
+  /** Index: assets__fungible_token_events_sorting_idx */
   emitted_in_shard_id: string;
 
   /**
    * Primary key. Index: assets__fungible_token_events_pkey
    * Index: assets__fungible_token_events_sorting_idx
-   * Index: assets__fungible_token_events_unique
    */
   emitted_index_of_event_entry_in_shard: number;
 
-  /**
-   * Index: assets__ft_contract_id_idx
-   * Primary key. Index: assets__fungible_token_events_pkey
-   */
+  /** Index: assets__ft_contract_id_idx */
   emitted_by_contract_account_id: string;
 
-  /** Primary key. Index: assets__fungible_token_events_pkey */
   amount: string;
 
-  /** Primary key. Index: assets__fungible_token_events_pkey */
   event_kind: FtEventKind;
 
   /**
    * Default value: ''::text
    * Index: assets__fungible_token_events_old_owner_account_id_idx
-   * Primary key. Index: assets__fungible_token_events_pkey
    */
   token_old_owner_account_id?: string;
 
   /**
    * Default value: ''::text
    * Index: assets__fungible_token_events_new_owner_account_id_idx
-   * Primary key. Index: assets__fungible_token_events_pkey
    */
   token_new_owner_account_id?: string;
 
-  /**
-   * Default value: ''::text
-   * Primary key. Index: assets__fungible_token_events_pkey
-   */
+  /** Default value: ''::text */
   event_memo?: string;
 }
--- backend/src/database/models/readOnlyIndexer/assets-non-fungible-token-events.ts
+++ backend/src/database/models/readOnlyIndexer/assets-non-fungible-token-events.ts
@@ -5,125 +5,83 @@ import NftEventKind from "./nft-event-kind";
 import { ReceiptsId } from "./receipts";
 
 export default interface AssetsNonFungibleTokenEvents {
-  /**
-   * Primary key. Index: assets__non_fungible_token_events_pkey
-   * Index: assets__non_fungible_token_events_unique
-   */
+  /** Primary key. Index: assets__non_fungible_token_events_pkey */
   emitted_for_receipt_id: ReceiptsId;
 
   /**
    * Index: assets__non_fungible_token_events_block_timestamp_idx
-   * Primary key. Index: assets__non_fungible_token_events_pkey
    * Index: assets__non_fungible_token_events_sorting_idx
    */
   emitted_at_block_timestamp: string;
 
-  /**
-   * Primary key. Index: assets__non_fungible_token_events_pkey
-   * Index: assets__non_fungible_token_events_sorting_idx
-   */
+  /** Index: assets__non_fungible_token_events_sorting_idx */
   emitted_in_shard_id: string;
 
   /**
    * Primary key. Index: assets__non_fungible_token_events_pkey
    * Index: assets__non_fungible_token_events_sorting_idx
-   * Index: assets__non_fungible_token_events_unique
    */
   emitted_index_of_event_entry_in_shard: number;
 
-  /**
-   * Index: assets__nft_contract_id_idx
-   * Primary key. Index: assets__non_fungible_token_events_pkey
-   */
+  /** Index: assets__nft_contract_id_idx */
   emitted_by_contract_account_id: string;
 
-  /** Primary key. Index: assets__non_fungible_token_events_pkey */
   token_id: string;
 
-  /** Primary key. Index: assets__non_fungible_token_events_pkey */
   event_kind: NftEventKind;
 
-  /**
-   * Index: assets__non_fungible_token_events_old_owner_account_id_idx
-   * Primary key. Index: assets__non_fungible_token_events_pkey
-   */
+  /** Index: assets__non_fungible_token_events_old_owner_account_id_idx */
   token_old_owner_account_id: string;
 
-  /**
-   * Index: assets__non_fungible_token_events_new_owner_account_id_idx
-   * Primary key. Index: assets__non_fungible_token_events_pkey
-   */
+  /** Index: assets__non_fungible_token_events_new_owner_account_id_idx */
   token_new_owner_account_id: string;
 
-  /** Primary key. Index: assets__non_fungible_token_events_pkey */
   token_authorized_account_id: string;
 
-  /** Primary key. Index: assets__non_fungible_token_events_pkey */
   event_memo: string;
 }
 
 export interface AssetsNonFungibleTokenEventsInitializer {
-  /**
-   * Primary key. Index: assets__non_fungible_token_events_pkey
-   * Index: assets__non_fungible_token_events_unique
-   */
+  /** Primary key. Index: assets__non_fungible_token_events_pkey */
   emitted_for_receipt_id: ReceiptsId;
 
   /**
    * Index: assets__non_fungible_token_events_block_timestamp_idx
-   * Primary key. Index: assets__non_fungible_token_events_pkey
    * Index: assets__non_fungible_token_events_sorting_idx
    */
   emitted_at_block_timestamp: string;
 
-  /**
-   * Primary key. Index: assets__non_fungible_token_events_pkey
-   * Index: assets__non_fungible_token_events_sorting_idx
-   */
+  /** Index: assets__non_fungible_token_events_sorting_idx */
   emitted_in_shard_id: string;
 
   /**
    * Primary key. Index: assets__non_fungible_token_events_pkey
    * Index: assets__non_fungible_token_events_sorting_idx
-   * Index: assets__non_fungible_token_events_unique
    */
   emitted_index_of_event_entry_in_shard: number;
 
-  /**
-   * Index: assets__nft_contract_id_idx
-   * Primary key. Index: assets__non_fungible_token_events_pkey
-   */
+  /** Index: assets__nft_contract_id_idx */
   emitted_by_contract_account_id: string;
 
-  /** Primary key. Index: assets__non_fungible_token_events_pkey */
   token_id: string;
 
-  /** Primary key. Index: assets__non_fungible_token_events_pkey */
   event_kind: NftEventKind;
 
   /**
    * Default value: ''::text
    * Index: assets__non_fungible_token_events_old_owner_account_id_idx
-   * Primary key. Index: assets__non_fungible_token_events_pkey
    */
   token_old_owner_account_id?: string;
 
   /**
    * Default value: ''::text
    * Index: assets__non_fungible_token_events_new_owner_account_id_idx
-   * Primary key. Index: assets__non_fungible_token_events_pkey
    */
   token_new_owner_account_id?: string;
 
-  /**
-   * Default value: ''::text
-   * Primary key. Index: assets__non_fungible_token_events_pkey
-   */
+  /** Default value: ''::text */
   token_authorized_account_id?: string;
 
-  /**
-   * Default value: ''::text
-   * Primary key. Index: assets__non_fungible_token_events_pkey
-   */
+  /** Default value: ''::text */
   event_memo?: string;
 }
